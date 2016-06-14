0ba7a51a1d661c194651105cec5919340232d83e
Joseph Lee <joseph.lee22590@gmail.com>
2016-06-13 19:13:01 -0700

    Bing Maps: Redstone executable is just maps.exe.

---
+++ addon/appModules/maps.py
@@ -0,0 +1,8 @@
+# WinTenApps/maps_windows.py
+# Part of Windows 10 App Essentials collection
+# Copyright 2016 Joseph Lee, released under GPL.
+
+# Numerous enhancements for Bing Maps app.
+# This is the Redstone version of the app (different executable name).
+
+from maps_windows import *
