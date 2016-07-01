146f52d414eebb9c781773696f5e137a87ec3819
Joseph Lee <joseph.lee22590@gmail.com>
2016-06-30 18:37:48 -0700

    Microsoft Edge: add the app module for microsoftedgecp.exe (build 14379 or later).

---
+++ addon/appModules/microsoftedgecp.py
@@ -0,0 +1,9 @@
+# MicrosoftEdge.py
+# Part of Windows 10 App Essentials collection
+# Copyright 2016 Joseph Lee, released under GPL
+
+# Core Edge support provided by NvDA Core (NVDAObjects/UIA package)
+# Provides additional enhancements.
+# A substitute app module for newer versions of Edge included in build 14379 and later.
+
+from microsoftedge import *
