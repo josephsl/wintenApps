26f1f0ae5256a71018e3bc1abd3f2d580963f44a
Joseph Lee <joseph.lee22590@gmail.com>
2016-08-02 21:23:33 -0700

    Bing Maps: Moved app module content around.
    As maps.exe is here to stay, move maps_windows content to maps.py. The old maps.windows.exe support will be phased out by end of 2016.

--- addon/appModules/maps_windows.py
+++ addon/appModules/maps_windows.py
@@ -3,38 +3,6 @@
 # Copyright 2016 Joseph Lee, released under GPL.
 
 # Numerous enhancements for Bing Maps app.
+# This is the Threshold version of the app (different executable name).
 
-import appModuleHandler
-import api
-import controlTypes
-import config
-from NVDAObjects.UIA import UIA
-import tones
-
-# Map locations
-# A static text denoting where one is located on the map.
-class MapLocation(UIA):
-	"""Plays a tone indicating the current map position."""
-
-	def event_becomeNavigatorObject(self):
-		l,t,w,h=self.location
-		x = l+(w/2)
-		y = t+(h/2)
-		screenWidth, screenHeight = api.getDesktopObject().location[2], api.getDesktopObject().location[3]
-		if x <= screenWidth or y <= screenHeight:
-			minPitch=config.conf['mouse']['audioCoordinates_minPitch']
-			maxPitch=config.conf['mouse']['audioCoordinates_maxPitch']
-			curPitch=minPitch+((maxPitch-minPitch)*((screenHeight-y)/float(screenHeight)))
-			brightness=config.conf['mouse']['audioCoordinates_maxVolume']
-			leftVolume=int((85*((screenWidth-float(x))/screenWidth))*brightness)
-			rightVolume=int((85*(float(x)/screenWidth))*brightness)
-			tones.beep(curPitch,40,left=leftVolume,right=rightVolume)
-		super(MapLocation,self).event_becomeNavigatorObject()
-
-
-class AppModule(appModuleHandler.AppModule):
-
-	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		if isinstance(obj, UIA):
-			if obj.role in (controlTypes.ROLE_STATICTEXT, controlTypes.ROLE_BUTTON) and obj.parent.parent.UIAElement.cachedClassName == "Map":
-				clsList.insert(0, MapLocation)
+from maps import *
