5039a450f7de7ca435e471d5d2994d40b1be5c32
Joseph Lee <joseph.lee22590@gmail.com>
2016-08-03 17:58:38 -0700

    Skype Preview: announce text chat when focused in the app.
    Thankfully, no display model to fiddle around... Also, text chats fire name change event, so it's the matter of locating the correct item and announcing it. Once it is working, the next step is to apply the same regexp as Desktop client, as chat info is quite verbose.

--- addon/appModules/skypeapp.py
+++ addon/appModules/skypeapp.py
@@ -11,9 +11,13 @@ from NVDAObjects.UIA import UIA
 class AppModule(appModuleHandler.AppModule):
 
 	def event_nameChange(self, obj, nextHandler):
-		if isinstance(obj, UIA) and obj.UIAElement.cachedClassName == "TextBlock" and obj.next is not None:
-			# Announce typing indicator (same as Skype for Desktop).
-			nextElement = obj.next.UIAElement
-			if nextElement.cachedClassName == "RichEditBox" and nextElement.cachedAutomationID == "ChatEditBox":
-				ui.message(obj.name if obj.name != "" else "Typing stopped")
+		if isinstance(obj, UIA):
+			uiElement = obj.UIAElement
+			if uiElement.cachedClassName == "TextBlock" and obj.next is not None:
+				# Announce typing indicator (same as Skype for Desktop).
+				nextElement = obj.next.UIAElement
+				if nextElement.cachedClassName == "RichEditBox" and nextElement.cachedAutomationID == "ChatEditBox":
+					ui.message(obj.name if obj.name != "" else "Typing stopped")
+			elif uiElement.cachedAutomationID == "Message" and uiElement.cachedClassName == "ListViewItem" and obj == obj.parent.lastChild:
+				ui.message(obj.name)
 		nextHandler()
