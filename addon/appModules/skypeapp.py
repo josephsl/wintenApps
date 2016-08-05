3fd092089206116a065225776ed59a848e6f460b
Joseph Lee <joseph.lee22590@gmail.com>
2016-08-05 13:30:28 -0700

    Skype Preview: partial return of Control+NVDA+number row to read chat history.
    Sometimes, we don't know where chat history can be found in object hierarchy, so use scanning technique to locate it. Also, chat history command will say 'history not found' if one opens other panels such as Settings.

--- addon/appModules/skypeapp.py
+++ addon/appModules/skypeapp.py
@@ -7,8 +7,14 @@
 import appModuleHandler
 import ui
 from NVDAObjects.UIA import UIA
+import api
 
 class AppModule(appModuleHandler.AppModule):
+
+	def __init__(self, *args, **kwargs):
+		super(AppModule, self).__init__(*args, **kwargs)
+		for pos in xrange(10):
+			self.bindGesture("kb:control+nvda+%s"%pos, "readMessage")
 
 	def event_nameChange(self, obj, nextHandler):
 		if isinstance(obj, UIA):
@@ -18,6 +24,23 @@ class AppModule(appModuleHandler.AppModule):
 				nextElement = obj.next.UIAElement
 				if nextElement.cachedClassName == "RichEditBox" and nextElement.cachedAutomationID == "ChatEditBox":
 					ui.message(obj.name if obj.name != "" else "Typing stopped")
-			elif uiElement.cachedAutomationID == "Message" and uiElement.cachedClassName == "ListViewItem" and obj == obj.parent.lastChild:
+			elif uiElement.cachedAutomationID == "Message" and uiElement.cachedClassName == "ListViewItem":
 				ui.message(obj.name)
 		nextHandler()
+
+	def script_readMessage(self, gesture):
+		# Foreground isn't reliable, so need to locate the actual chat contents list.
+		fg = api.getForegroundObject()
+		if fg.getChild(1).childCount > 0:
+			screenContent = fg.getChild(1)
+		else:
+			screenContent = fg.getChild(2)
+		# Position of chat history in object hierarchy changes based on which tabv is active.
+		# Wish there is a more elegant way to do this...
+		for element in screenContent.children:
+			if isinstance(element, UIA) and element.UIAElement.cachedAutomationID == "chatMessagesListView":
+				pos = int(gesture.displayName[-1])
+				if pos == 0: pos += 10
+				ui.message(element.getChild(0-pos).name)
+				return
+		ui.message("Chat history not found")
