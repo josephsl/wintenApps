9357e773fa5e3a125e5aa144621610e8e46f0389
derekriemer <derek.riemer@colorado.edu>
2016-08-22 09:47:24 -0600

    Weather: Make settings a button, fix possible error if UIA doesn't exist for obj, make feedback report as a tab

--- addon/appModules/microsoft_msn_weather.py
+++ addon/appModules/microsoft_msn_weather.py
@@ -5,17 +5,26 @@
 # Provides workarounds for the weather app.
 
 
-import appModuleHandler
-import controlTypes
 import re
+import controlTypes
+import appModuleHandler
+from NVDAObjects import NVDAObject
 
 #Regexp for deciding whether this ID should be a tab control
 RE_TAB_AUTOMATION_MATCH = re.compile(r"L1NavigationButton_\d+")
+#Regexp for deciding if this should be a button
+RE_BUTTONCONTROL = re.compile("|".join([
+	r"L1NavigationButton_Settings",
+]))
 
 class AppModule(appModuleHandler.AppModule):
 	def event_NVDAObject_init(self, obj):
+		try:
+			
 		if obj.UIAElement.CachedAutomationId == u"SideNavigationBar" and not obj.role in (controlTypes.ROLE_GROUPING, ):
 			obj.role = controlTypes.ROLE_TABCONTROL
 		theId = obj.UIAElement.CachedAutomationId
 		if RE_TAB_AUTOMATION_MATCH.match(theId):
 			obj.role = controlTypes.ROLE_TAB
+		elif RE_BUTTONCONTROL.match(theId):
+			obj.role = controlTypes.ROLE_BUTTON
