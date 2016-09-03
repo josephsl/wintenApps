43d1cced90876503f8b7f0656bfc67471b1cad72
Joseph Lee <joseph.lee22590@gmail.com>
2016-09-03 00:50:18 -0700

    Calendar: suppress read-only state from appointment subject.
    A variant of Mail app: appointment subject exposes anoying read-only state, thus suppress it as well.

---
+++ addon/appModules/hxcalendarappimm.py
@@ -0,0 +1,25 @@
+# WinTenApps/hxcalendarappimm.py
+# Part of Windows 10 App Essentials collection
+# Copyright 2016 Joseph Lee, released under GPL.
+
+# Fixes for certain issues with Windows 10 Calendar app (based on Outlook 2016).
+
+import appModuleHandler
+import controlTypes
+from NVDAObjects.UIA import UIA
+
+# Suppress read-only state announcement (quite anoying).
+class AppointmentSubject(UIA):
+
+	def _get_states(self):
+		states = super(AppointmentSubject, self).states
+		states.discard(controlTypes.STATE_READONLY)
+		return states
+
+class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if isinstance(obj, UIA):
+			# Very redundant to say "read-only" when we do know messages are read-only.
+			if obj.UIAElement.cachedAutomationID == "QuickItemSubject":
+					clsList.insert(0, AppointmentSubject)
