8b5b625f96e65075690fcafa55c2f802837dbc7a
Joseph Lee <joseph.lee22590@gmail.com>
2016-09-23 17:16:04 -0700

    Installer: made version detection dialog messages translatable, move version name to a separate variable to ease translation burden.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -10,5 +10,9 @@ import gui
 import wx
 
 def onInstall():
-	if sys.getwindowsversion().build < 7601 and gui.messageBox("You are using an older version of Windows. This add-on requires Windows 7 Service Pack 1 or later. Are you sure you wish to install this add-on anyway?", "Old Windows version", wx.YES | wx.NO | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION) == wx.NO:
+	requiredVer = "Windows 7 Service Pack 1"
+	# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows (minSupportedVersion is the minimum version required for this add-on)..
+	if sys.getwindowsversion().build < 7601 and gui.messageBox(_("You are using an older version of Windows. This add-on requires {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
+	# Translators: title of the dialog shown when attempting to install the add-on on an old version of Windows.
+	_("Old Windows version"), wx.YES | wx.NO | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION) == wx.NO:
 		raise RuntimeError("Old Windows version detected")
