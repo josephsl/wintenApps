970261fb5789e31cf7da0831de83c5be67bdc148
Joseph Lee <joseph.lee22590@gmail.com>
2016-11-02 09:06:59 -0700

    Windows Store: support Store released in November 2016 (winstore.app.exe).
    Yet another reason for developing this add-on: executable name changed, this time involving Windows Store, thus a new module is available to handle this case.

---
+++ addon/appModules/winstore_app.py
@@ -0,0 +1,8 @@
+# Windows 10 Store
+# Part of Windows 10 App Essentials
+# Copyright 2016 Joseph Lee, released under GPL
+
+# Enhancements to support Windows Store.
+# New app module as of November 2016.
+
+from winstore_mobile import *
