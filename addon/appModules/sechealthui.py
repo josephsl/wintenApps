f0e63d0ef7b4459e330b7079eda945ac5a11e13c
Joseph Lee <joseph.lee22590@gmail.com>
2016-12-07 21:32:48 -0800

    Windows Defender UWP: Add a workaround to let NVDA announce button labels.
    Build 14986 and later: similar to Twitter and other app modules, current version of Windows Defender UWP has no labels for certain buttons. Thankfully, the last child of this control has the labels we need, so fetch this instead.
    This is a temporary solution and will be removed once correct labels from Microsoft are released and implemented correctly.

---
+++ addon/appModules/sechealthui.py
@@ -0,0 +1,19 @@
+# WinTenApps/SecHealthUI.py
+# Part of Windows 10 App Essentials collection
+# Copyright 2016 Joseph Lee, released under GPL.
+
+# Workarounds for Windows Defender UWP (build 14986 and later).
+
+import appModuleHandler
+import controlTypes
+from NVDAObjects.UIA import UIA
+
+class AppModule(appModuleHandler.AppModule):
+
+	def event_NVDAObject_init(self, obj):
+		# Until Microsoft fixes this...
+		if isinstance(obj, UIA) and obj.role == controlTypes.ROLE_BUTTON and obj.name == "":
+			try:
+				obj.name = obj.lastChild.name
+			except AttributeError:
+				pass
