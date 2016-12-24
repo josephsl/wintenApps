855856695f2fa0b335aad7d8a37601416efd04d7
Joseph Lee <joseph.lee22590@gmail.com>
2016-12-23 18:35:39 -0800

    Windows Store: recognize the new app module as the official version.

--- addon/appModules/winstore_app.py
+++ addon/appModules/winstore_app.py
@@ -3,6 +3,15 @@
 # Copyright 2016 Joseph Lee, released under GPL
 
 # Enhancements to support Windows Store.
-# New app module as of November 2016.
 
-from winstore_mobile import *
+import appModuleHandler
+import ui
+import controlTypes
+from NVDAObjects.UIA import UIA
+
+class AppModule(appModuleHandler.AppModule):
+
+	def event_NVDAObject_init(self, obj):
+		# Extraneous information announced when going through apps to be updated/installed, so use a grandchild's name.
+		if isinstance(obj, UIA) and obj.role == controlTypes.ROLE_LISTITEM and obj.firstChild and obj.firstChild.UIAElement.cachedAutomationID == "InstallControl":
+			obj.name = obj.firstChild.firstChild.name
