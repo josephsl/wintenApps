7dbe571b7ad8daaa3499810ec1aeccb86ed0524b
Joseph Lee <joseph.lee22590@gmail.com>
2017-01-20 15:58:34 -0800

    Windows Store: announce app and other content download progress.
    While an app or content is downloaindg, a progress text is shown that provides useful info for users to hear, including download status, size and so on. However, because it also suffers from cache issue (same thing spoken multiple times), provide a cache so different info can be announced. Also, product title will be announced along with the progress text.

--- addon/appModules/winstore_app.py
+++ addon/appModules/winstore_app.py
@@ -8,6 +8,7 @@ import appModuleHandler
 import ui
 import controlTypes
 from NVDAObjects.UIA import UIA
+from NVDAObjects.behaviors import ProgressBar
 
 class AppModule(appModuleHandler.AppModule):
 
@@ -17,6 +18,27 @@ class AppModule(appModuleHandler.AppModule):
 			obj.name = obj.firstChild.firstChild.name
 
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		if isinstance(obj, UIA) and obj.UIAElement.cachedAutomationID == "SearchTextBox":
-			import globalPlugins.wintenObjs
-			clsList.insert(0, globalPlugins.wintenObjs.SearchField)
+		if isinstance(obj, UIA):
+			if obj.UIAElement.cachedAutomationID == "SearchTextBox":
+				import globalPlugins.wintenObjs
+				clsList.insert(0, globalPlugins.wintenObjs.SearchField)
+
+	def event_valueChange(self, obj, nextHandler):
+		if isinstance(obj, UIA):
+			# Detect change in app installation progress bar.
+			if obj.UIAElement.cachedAutomationID == "_progressBar":
+				obj.event_valueChange()
+		nextHandler()
+
+	# just like Settings app, have a cache of download progress text handy.
+	_appInstallProgress = ""
+
+	def event_nameChange(self, obj, nextHandler):
+		if isinstance(obj, UIA) and obj.UIAElement.cachedAutomationID == "_progressText":
+			if obj.name != self._appInstallProgress:
+				self._appInstallProgress = obj.name
+				# Don't forget to announce product title.
+				productTitle = obj.parent.previous
+				if productTitle.UIAElement.cachedAutomationID == "_productTitle":
+					ui.message(" ".join([productTitle.name, obj.name]))
+		nextHandler()
