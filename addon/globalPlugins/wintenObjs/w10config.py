66ecab07b436a636a74c2a0fef31bd089775bd89
Joseph Lee <joseph.lee22590@gmail.com>
2017-01-26 12:53:12 -0800

    Update checker: let installed copies of NVDA download add-on updates on its own. re #20.

--- addon/globalPlugins/wintenObjs/w10config.py
+++ addon/globalPlugins/wintenObjs/w10config.py
@@ -9,12 +9,19 @@
 import os
 import threading
 import urllib
+import tempfile
+import hashlib
+import ctypes.wintypes
+import ssl
+import shellapi
 import time
 import re
 import config
 import gui
 import wx
 import addonHandler
+import updateCheck as coreUpdateCheck
+import winUser
 
 # Add-on config database
 confspec = {
@@ -90,12 +97,12 @@ def updateCheck(autoCheck=False):
 
 def getUpdateResponse(message, caption, updateURL):
 	if gui.messageBox(message, caption, wx.YES_NO | wx.NO_DEFAULT | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION) == wx.YES:
-		os.startfile(updateURL)
+		W10UpdateDownloader([updateURL]).start() if config.isInstalledCopy() else os.startfile(updateURL)
 
 class WinTenAppsConfigDialog(wx.Dialog):
 
 	def __init__(self, parent):
-		# Translators: The title of a dialog to configure advanced SPL add-on options such as update checking.
+		# Translators: The title of a dialog to configure advanced WinTenApps add-on options such as update checking.
 		super(WinTenAppsConfigDialog, self).__init__(parent, title=_("Windows 10 App Essentials settings"))
 
 		mainSizer = wx.BoxSizer(wx.VERTICAL)
@@ -149,9 +156,164 @@ class WinTenAppsConfigDialog(wx.Dialog):
 		progressDialog = gui.IndeterminateProgressDialog(gui.mainFrame,
 		# Translators: The title of the dialog presented while checking for add-on updates.
 		_("Add-on update check"),
-		# Translators: The message displayed while checking for newer version of Studio add-on.
+		# Translators: The message displayed while checking for newer version of WinTenApps add-on.
 		_("Checking for new version of Windows 10 App Essentials add-on..."))
 		threading.Thread(target=updateCheck).start()
 
 def onConfigDialog(evt):
 	gui.mainFrame._popupSettingsDialog(WinTenAppsConfigDialog)
+
+
+# Update downloader (credit: NV Access)
+# Customized for WinTenApps add-on.
+
+#: The download block size in bytes.
+DOWNLOAD_BLOCK_SIZE = 8192 # 8 kb
+
+def checkForUpdate(auto=False):
+	"""Check for an updated version of NVDA.
+	This will block, so it generally shouldn't be called from the main thread.
+	@param auto: Whether this is an automatic check for updates.
+	@type auto: bool
+	@return: Information about the update or C{None} if there is no update.
+	@rtype: dict
+	@raise RuntimeError: If there is an error checking for an update.
+	"""
+	params = {
+		"autoCheck": auto,
+		"version": versionInfo.version,
+		"versionType": versionInfo.updateVersionType,
+		"osVersion": winVersion.winVersionText,
+		"x64": os.environ.get("PROCESSOR_ARCHITEW6432") == "AMD64",
+		"language": languageHandler.getLanguage(),
+		"installed": config.isInstalledCopy(),
+	}
+	url = "%s?%s" % (CHECK_URL, urllib.urlencode(params))
+	try:
+		res = urllib.urlopen(url)
+	except IOError as e:
+		if isinstance(e.strerror, ssl.SSLError) and e.strerror.reason == "CERTIFICATE_VERIFY_FAILED":
+			# #4803: Windows fetches trusted root certificates on demand.
+			# Python doesn't trigger this fetch (PythonIssue:20916), so try it ourselves
+			_updateWindowsRootCertificates()
+			# and then retry the update check.
+			res = urllib.urlopen(url)
+		else:
+			raise
+	if res.code != 200:
+		raise RuntimeError("Checking for update failed with code %d" % res.code)
+	info = {}
+	for line in res:
+		line = line.rstrip()
+		try:
+			key, val = line.split(": ", 1)
+		except ValueError:
+			raise RuntimeError("Error in update check output")
+		info[key] = val
+	if not info:
+		return None
+	return info
+
+
+class W10UpdateDownloader(coreUpdateCheck.UpdateDownloader):
+	"""Overrides NVDA Core's downloader.)
+	No hash checking for now, and URL's and temp file paths are different.
+	"""
+
+	def __init__(self, urls, fileHash=None):
+		"""Constructor.
+		@param urls: URLs to try for the update file.
+		@type urls: list of str
+		@param fileHash: The SHA-1 hash of the file as a hex string.
+		@type fileHash: basestring
+		"""
+		super(W10UpdateDownloader, self).__init__(urls, fileHash)
+		self.urls = urls
+		self.destPath = tempfile.mktemp(prefix="stationPlaylist_update-", suffix=".nvda-addon")
+		self.fileHash = fileHash
+
+	def start(self):
+		"""Start the download.
+		"""
+		self._shouldCancel = False
+		# Use a timer because timers aren't re-entrant.
+		self._guiExecTimer = wx.PyTimer(self._guiExecNotify)
+		gui.mainFrame.prePopup()
+		# Translators: The title of the dialog displayed while downloading add-on update.
+		self._progressDialog = wx.ProgressDialog(_("Downloading Add-on Update"),
+			# Translators: The progress message indicating that a connection is being established.
+			_("Connecting"),
+			# PD_AUTO_HIDE is required because ProgressDialog.Update blocks at 100%
+			# and waits for the user to press the Close button.
+			style=wx.PD_CAN_ABORT | wx.PD_ELAPSED_TIME | wx.PD_REMAINING_TIME | wx.PD_AUTO_HIDE,
+			parent=gui.mainFrame)
+		self._progressDialog.Raise()
+		t = threading.Thread(target=self._bg)
+		t.daemon = True
+		t.start()
+
+	def _error(self):
+		self._stopped()
+		gui.messageBox(
+			# Translators: A message indicating that an error occurred while downloading an update to NVDA.
+			_("Error downloading add-on update."),
+			_("Error"),
+			wx.OK | wx.ICON_ERROR)
+
+	def _downloadSuccess(self):
+		self._stopped()
+		# Translators: The message presented when the update has been successfully downloaded
+		# and is about to be installed.
+		gui.messageBox(_("Add-on update downloaded. It will now be installed."),
+			# Translators: The title of the dialog displayed when the update is about to be installed.
+			_("Install Add-on Update"))
+		# #4475: ensure that the new process shows its first window, by providing SW_SHOWNORMAL
+		shellapi.ShellExecute(None, None,
+			self.destPath.decode("mbcs"),
+			None, None, winUser.SW_SHOWNORMAL)
+
+
+# These structs are only complete enough to achieve what we need.
+class CERT_USAGE_MATCH(ctypes.Structure):
+	_fields_ = (
+		("dwType", ctypes.wintypes.DWORD),
+		# CERT_ENHKEY_USAGE struct
+		("cUsageIdentifier", ctypes.wintypes.DWORD),
+		("rgpszUsageIdentifier", ctypes.c_void_p), # LPSTR *
+	)
+
+class CERT_CHAIN_PARA(ctypes.Structure):
+	_fields_ = (
+		("cbSize", ctypes.wintypes.DWORD),
+		("RequestedUsage", CERT_USAGE_MATCH),
+		("RequestedIssuancePolicy", CERT_USAGE_MATCH),
+		("dwUrlRetrievalTimeout", ctypes.wintypes.DWORD),
+		("fCheckRevocationFreshnessTime", ctypes.wintypes.BOOL),
+		("dwRevocationFreshnessTime", ctypes.wintypes.DWORD),
+		("pftCacheResync", ctypes.c_void_p), # LPFILETIME
+		("pStrongSignPara", ctypes.c_void_p), # PCCERT_STRONG_SIGN_PARA
+		("dwStrongSignFlags", ctypes.wintypes.DWORD),
+	)
+
+# Borrowed from NVDA Core (the only difference is the URL).
+def _updateWindowsRootCertificates():
+	crypt = ctypes.windll.crypt32
+	# Get the server certificate.
+	sslCont = ssl._create_unverified_context()
+	u = urllib.urlopen("https://www.nvaccess.org/nvdaUpdateCheck", context=sslCont)
+	cert = u.fp._sock.getpeercert(True)
+	u.close()
+	# Convert to a form usable by Windows.
+	certCont = crypt.CertCreateCertificateContext(
+		0x00000001, # X509_ASN_ENCODING
+		cert,
+		len(cert))
+	# Ask Windows to build a certificate chain, thus triggering a root certificate update.
+	chainCont = ctypes.c_void_p()
+	crypt.CertGetCertificateChain(None, certCont, None, None,
+		ctypes.byref(CERT_CHAIN_PARA(cbSize=ctypes.sizeof(CERT_CHAIN_PARA),
+			RequestedUsage=CERT_USAGE_MATCH())),
+		0, None,
+		ctypes.byref(chainCont))
+	crypt.CertFreeCertificateChain(chainCont)
+	crypt.CertFreeCertificateContext(certCont)
