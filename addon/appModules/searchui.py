09fc52193bdcc0478a75c35b76bcb9ae857b3c60
Joseph Lee <joseph.lee22590@gmail.com>
2017-03-01 16:01:08 -0800

    SearchUI: do not let description for suggestions be announced if it is the same as the label. re #21.
    Perhaps due to an updated Cortana: do not let descriptions be announced if it is the same as the suggestion label.

--- addon/appModules/searchui.py
+++ addon/appModules/searchui.py
@@ -7,10 +7,47 @@
 # Extended to let NVDA cooperate with Cortana.
 
 import sys
+import appModuleHandler
+import controlTypes
+import api
+import speech
 import ui
-from nvdaBuiltin.appModules.searchui import *
+import config
+from NVDAObjects.UIA import UIA
+from NVDAObjects.UIA.edge import EdgeList
+from NVDAObjects.IAccessible import IAccessible, ContentGenericClient
 
-class AppModule(AppModule):
+# Windows 10 Search UI suggestion list item
+class SuggestionListItem(UIA):
+
+	role=controlTypes.ROLE_LISTITEM
+
+	def event_UIA_elementSelected(self):
+		focusControllerFor=api.getFocusObject().controllerFor
+		if len(focusControllerFor)>0 and focusControllerFor[0].appModule is self.appModule and self.name:
+			speech.cancelSpeech()
+			api.setNavigatorObject(self)
+			self.reportFocus()
+
+	def reportFocus(self):
+		# #21: nullify description if it is the same as the item label.
+		if self.description == self.name and config.conf["presentation"]["reportObjectDescriptions"]:
+			self.description = None
+		super(SuggestionListItem, self).reportFocus()
+
+
+class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self,obj,clsList):
+		if isinstance(obj,UIA) and isinstance(obj.parent,EdgeList):
+			clsList.insert(0,SuggestionListItem)
+		elif isinstance(obj,IAccessible):
+			try:
+				# #5288: Never use ContentGenericClient, as this uses displayModel
+				# which will freeze if the process is suspended.
+				clsList.remove(ContentGenericClient)
+			except ValueError:
+				pass
 
 	# Past responses from Cortana (cached to prevent repetition, initially an empty string).
 	CortanaResponseCache = ""
