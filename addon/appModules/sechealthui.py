c785421f98a83cc50e6b738ef21b3169027ca70f
Joseph Lee <joseph.lee22590@gmail.com>
2017-03-08 12:23:23 -0800

    Windows Defender UWP: removed support for builds earlier than 15007.
    now that slow ring Insiders are at 15048, it is pointless to keep 14986 code around.

--- addon/appModules/sechealthui.py
+++ addon/appModules/sechealthui.py
@@ -1,13 +1,12 @@
 # WinTenApps/SecHealthUI.py
 # Part of Windows 10 App Essentials collection
-# Copyright 2016 Joseph Lee, released under GPL.
+# Copyright 2016-2017 Joseph Lee, released under GPL.
 
 # Workarounds for Windows Defender UWP (build 14986 and later).
 
 import appModuleHandler
 import controlTypes
 from NVDAObjects.UIA import UIA
-import winVersion
 
 class AppModule(appModuleHandler.AppModule):
 
@@ -16,6 +15,6 @@ class AppModule(appModuleHandler.AppModule):
 		# Oddly enough, in build 15007 and later, button label is not the last child, but the one before that.
 		if isinstance(obj, UIA) and obj.role == controlTypes.ROLE_BUTTON and obj.name == "":
 			try:
-				obj.name = obj.getChild(-2).name if winVersion.winVersion.build > 15000 else obj.lastChild.name
+				obj.name = obj.getChild(-2).name
 			except AttributeError:
 				pass
