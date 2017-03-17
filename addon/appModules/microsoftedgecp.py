6ee0dfadb523b8765b2d00ea5d86767cfbb37252
Joseph Lee <joseph.lee22590@gmail.com>
2017-03-17 02:43:33 -0700

    Edge CP: incorporate web content view announcement workaround.
    Because of web content view announcement, an app module for Edge CP is now part of NVDA Core that suppresses this, thus incorporate this into WinTenApps version.

--- addon/appModules/microsoftedgecp.py
+++ addon/appModules/microsoftedgecp.py
@@ -1,9 +1,14 @@
 # MicrosoftEdge.py
 # Part of Windows 10 App Essentials collection
-# Copyright 2016 Joseph Lee, released under GPL
+# Copyright 2016-2017 Joseph Lee, released under GPL
 
 # Core Edge support provided by NvDA Core (NVDAObjects/UIA package)
 # Provides additional enhancements.
 # A substitute app module for newer versions of Edge included in build 14379 and later.
 
+# NVDA Core issue 6948: NVDA Core includes an app module for this app with web content view announcement workaround (Redstone 2).
+try:
+	from nvdaBuiltin.appModules.microsoftedgecp import *
+except ImportError:
+	pass
 from microsoftedge import *
