4c2a06208951f6968672f1a9e429d679e4933ca1
Joseph Lee <joseph.lee22590@gmail.com>
2017-05-11 01:41:47 -0700

    Windows Store: detect if SearchField is part of NVDA Core and never invoke overlay class chooser if it is.

--- addon/appModules/winstore_app.py
+++ addon/appModules/winstore_app.py
@@ -7,24 +7,25 @@
 import appModuleHandler
 import ui
 import controlTypes
-from NVDAObjects.UIA import UIA
+import NVDAObjects.UIA
 from NVDAObjects.behaviors import ProgressBar
 
 class AppModule(appModuleHandler.AppModule):
 
 	def event_NVDAObject_init(self, obj):
 		# Extraneous information announced when going through apps to be updated/installed, so use a grandchild's name.
-		if isinstance(obj, UIA) and obj.role == controlTypes.ROLE_LISTITEM and obj.firstChild and obj.firstChild.UIAElement.cachedAutomationID == "InstallControl":
+		if isinstance(obj, NVDAObjects.UIA.UIA) and obj.role == controlTypes.ROLE_LISTITEM and obj.firstChild and obj.firstChild.UIAElement.cachedAutomationID == "InstallControl":
 			obj.name = obj.firstChild.firstChild.name
 
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		if isinstance(obj, UIA):
+		# This whole thing (function) will be gone in NVDA 2017.3.
+		if not hasattr(NVDAObjects.UIA, "SearchField") and isinstance(obj, NVDAObjects.UIA.UIA):
 			if obj.UIAElement.cachedAutomationID == "SearchTextBox":
 				import globalPlugins.wintenObjs
 				clsList.insert(0, globalPlugins.wintenObjs.SearchField)
 
 	def event_valueChange(self, obj, nextHandler):
-		if isinstance(obj, UIA):
+		if isinstance(obj, NVDAObjects.UIA.UIA):
 			# Detect change in app installation progress bar.
 			if obj.UIAElement.cachedAutomationID == "_progressBar":
 				obj.event_valueChange()
@@ -34,7 +35,7 @@ class AppModule(appModuleHandler.AppModule):
 	_appInstallProgress = ""
 
 	def event_nameChange(self, obj, nextHandler):
-		if isinstance(obj, UIA) and obj.UIAElement.cachedAutomationID == "_progressText":
+		if isinstance(obj, NVDAObjects.UIA.UIA) and obj.UIAElement.cachedAutomationID == "_progressText":
 			if obj.name != self._appInstallProgress:
 				self._appInstallProgress = obj.name
 				# Don't forget to announce product title.
