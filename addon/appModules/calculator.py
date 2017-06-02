ef6ee112f86b86cfec2950ffc6755d3e97bedadc
Joseph Lee <joseph.lee22590@gmail.com>
2017-06-01 20:36:31 -0700

    Calculator: pressing Escape will now allow NVDA to announce display value of 0.
    Instead of just allowing ENTER key, Escape key should also be allowed, as it allows 'display is 0' to be announced.

--- addon/appModules/calculator.py
+++ addon/appModules/calculator.py
@@ -1,6 +1,6 @@
 # WinTenApps/calculator.py
 # Part of Windows 10 App Essentials collection
-# Copyright 2015-2016 Joseph Lee, released under GPL.
+# Copyright 2015-2017 Joseph Lee, released under GPL.
 
 # Provides enhanced support for modern Calculator, including announcing results.
 
@@ -15,26 +15,26 @@ import queueHandler
 class CalculatorResult(UIA):
 
 	def event_nameChange(self):
-		if not self.appModule.enterPressed:
+		if not self.appModule.shouldAnnounceResult:
 			return
 		else:
-			self.appModule.enterPressed = False
+			self.appModule.shouldAnnounceResult = False
 
 
 class AppModule(appModuleHandler.AppModule):
 
-	enterPressed = False
+	shouldAnnounceResult = False
 
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
 		if isinstance(obj, UIA):
-			# To handle calculator result announcement in Threshold and Redstone.
+			# To handle calculator result announcement.
 			if obj.UIAElement.cachedAutomationID == "CalculatorResults":
 				clsList.insert(0, CalculatorResult)
 
-	def script_enter(self, gesture):
+	def script_calculatorResult(self, gesture):
 		gesture.send()
 		# In redstone, calculator result keeps firing name change, so tell it to do so if and only if enter has been pressed.
-		self.enterPressed = True
+		self.shouldAnnounceResult = True
 		# Hack: only announce display text when an actual calculator button (usually equals button) is pressed.
 		focus = api.getFocusObject()
 		# In redstone, pressing enter does not move focus to equals button.
@@ -49,6 +49,7 @@ class AppModule(appModuleHandler.AppModule):
 				queueHandler.queueFunction(queueHandler.eventQueue, result.setFocus)
 
 	__gestures={
-		"kb:enter":"enter",
-		"kb:numpadEnter":"enter"
+		"kb:enter":"calculatorResult",
+		"kb:numpadEnter":"calculatorResult",
+		"kb:escape":"calculatorResult"
 	}
