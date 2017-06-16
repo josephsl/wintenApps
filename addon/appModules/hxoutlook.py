c22760ac1d4898902e0072ff93741a0aef2596d1
Joseph Lee <joseph.lee22590@gmail.com>
2017-06-15 23:06:24 -0700

    WinTenApps 17.06.2A patch, alias for hxmail (hxoutlook) created.
    Yet again, Microsoft has changed the name of the executable, this time changing from hxmail to hxoutlook.

---
+++ addon/appModules/hxoutlook.py
@@ -0,0 +1,9 @@
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2016 NV Access Limited
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+# Extended in 2017 by Joseph Lee
+
+"""An appModule for the Windows 10 Mail app"""
+
+from hxmail import *
