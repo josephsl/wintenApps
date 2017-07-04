740a14d44f5610c9da04de1476010d563b28f8f1
Joseph Lee <joseph.lee22590@gmail.com>
2017-07-04 13:52:37 -0700

    Edge: remove name change handler, add system alert event handler to deal with website alerts.

--- addon/appModules/microsoftedge.py
+++ addon/appModules/microsoftedge.py
@@ -13,25 +13,18 @@ import ui
 
 class AppModule(appModuleHandler.AppModule):
 
-	def event_nameChange(self, obj, nextHandler):
-		# Some objects do raise live region change event as well as name change.
-		if isinstance(obj, UIA) and not hasattr(obj, "event_liveRegionChange"):
-			# Notifications such as file download prompt.
-			if obj.role == controlTypes.ROLE_STATICTEXT and obj.parent.UIAElement.cachedClassName == "NotificationBar":
-				ui.message(obj.name)
-			# Accessibility message alerts.
-			elif obj.role == controlTypes.ROLE_ALERT and obj.UIAElement.cachedAutomationID == "a11y-announcements-message":
-				ui.message(obj.firstChild.name)
-		nextHandler()
-
-	event_UIA_liveRegionChanged = event_nameChange
-	# Live region change is part of NVDA 2017.3, so try catching this as well.
 	def event_liveRegionChange(self, obj, nextHandler):
 		if isinstance(obj, UIA):
 			# Accessibility message alerts.
-			# Return immediately after doing the following, otherwise double speaking results.
+			# For some messages, system alert event is raised along with this event, so return.
+			if obj.role == controlTypes.ROLE_ALERT and obj.UIAElement.cachedAutomationID == "a11y-announcements-message":
+				return
+		nextHandler()
+
+	# For message alerts, system alert and live region changed are fired at the same time.
+	def event_UIA_systemAlert(self, obj, nextHandler):
+		if isinstance(obj, UIA):
 			if obj.role == controlTypes.ROLE_ALERT and obj.UIAElement.cachedAutomationID == "a11y-announcements-message":
 				# Mick Curran says use last child in case a series of live texts are part of this control.
 				ui.message(obj.lastChild.name)
-				return
 		nextHandler()
