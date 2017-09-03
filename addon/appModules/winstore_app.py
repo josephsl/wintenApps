5516141560523af45d047a0a2c76721ffbccff13
Joseph Lee <joseph.lee22590@gmail.com>
2017-09-03 12:36:48 -0700

    Store app module: just use UIA from NVDAObjects.UIA, remove overlay class as it is now part of NVDA 2017.3.
    With the introduction of search field class in NVDA 2017.3, there's no need to have a Store version of search field handler. Also, to make life easier, just import UIA from NVDAObjects.UIA.

--- addon/appModules/winstore_app.py
+++ addon/appModules/winstore_app.py
@@ -7,8 +7,7 @@
 import appModuleHandler
 import ui
 import controlTypes
-import NVDAObjects.UIA
-from NVDAObjects.behaviors import ProgressBar
+from NVDAObjects.UIA import UIA
 
 class AppModule(appModuleHandler.AppModule):
 
@@ -17,25 +16,11 @@ class AppModule(appModuleHandler.AppModule):
 		if isinstance(obj, NVDAObjects.UIA.UIA) and obj.role == controlTypes.ROLE_LISTITEM and obj.firstChild and obj.firstChild.UIAElement.cachedAutomationID == "InstallControl":
 			obj.name = obj.firstChild.firstChild.name
 
-	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		# This whole thing (function) will be gone in NVDA 2017.3.
-		if not hasattr(NVDAObjects.UIA, "SearchField") and isinstance(obj, NVDAObjects.UIA.UIA):
-			if obj.UIAElement.cachedAutomationID == "SearchTextBox":
-				import globalPlugins.wintenObjs
-				clsList.insert(0, globalPlugins.wintenObjs.SearchField)
-
-	def event_valueChange(self, obj, nextHandler):
-		if isinstance(obj, NVDAObjects.UIA.UIA):
-			# Detect change in app installation progress bar.
-			if obj.UIAElement.cachedAutomationID == "_progressBar":
-				obj.event_valueChange()
-		nextHandler()
-
 	# just like Settings app, have a cache of download progress text handy.
 	_appInstallProgress = ""
 
 	def event_liveRegionChange(self, obj, nextHandler):
-		if isinstance(obj, NVDAObjects.UIA.UIA) and obj.UIAElement.cachedAutomationID == "_progressText":
+		if isinstance(obj, UIA) and obj.UIAElement.cachedAutomationID == "_progressText":
 			if obj.name != self._appInstallProgress:
 				self._appInstallProgress = obj.name
 				# Don't forget to announce product title.
@@ -45,7 +30,7 @@ class AppModule(appModuleHandler.AppModule):
 					# But since September 2017 update, the title is next door.
 					possibleProductTitle = productTitle.parent.previous
 					productTitle = productTitle.previous if possibleProductTitle is None else productTitle.parent.previous
-				if productTitle and isinstance(productTitle, NVDAObjects.UIA.UIA) and productTitle.UIAElement.cachedAutomationID == "_productTitle":
+				if productTitle and isinstance(productTitle, UIA) and productTitle.UIAElement.cachedAutomationID == "_productTitle":
 					ui.message(" ".join([productTitle.name, obj.name]))
 			return
 		nextHandler()
