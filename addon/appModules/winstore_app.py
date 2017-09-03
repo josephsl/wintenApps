fc766188c62d3f425f19bb9b4f3e92cf2c5df62c
Joseph Lee <joseph.lee22590@gmail.com>
2017-09-03 12:40:14 -0700

    Store: oops, NVDAObjects.UIA should not have been part of event_NvDAObject-init, oversight.

--- addon/appModules/winstore_app.py
+++ addon/appModules/winstore_app.py
@@ -13,7 +13,7 @@ class AppModule(appModuleHandler.AppModule):
 
 	def event_NVDAObject_init(self, obj):
 		# Extraneous information announced when going through apps to be updated/installed, so use a grandchild's name.
-		if isinstance(obj, NVDAObjects.UIA.UIA) and obj.role == controlTypes.ROLE_LISTITEM and obj.firstChild and obj.firstChild.UIAElement.cachedAutomationID == "InstallControl":
+		if isinstance(obj, UIA) and obj.role == controlTypes.ROLE_LISTITEM and obj.firstChild and obj.firstChild.UIAElement.cachedAutomationID == "InstallControl":
 			obj.name = obj.firstChild.firstChild.name
 
 	# just like Settings app, have a cache of download progress text handy.
