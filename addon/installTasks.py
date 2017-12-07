52141603892752573b988d24ab566fb28cbc9bb2
Joseph Lee <joseph.lee22590@gmail.com>
2017-12-06 19:31:06 -0800

    Installer: Version 1703 is required

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -12,9 +12,9 @@ import addonHandler
 addonHandler.initTranslation()
 
 def onInstall():
-	requiredVer = "Windows 10 Version 1607"
+	requiredVer = "Windows 10 Version 1703"
 	# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows (minSupportedVersion is the minimum version required for this add-on).
-	if sys.getwindowsversion().build < 14393 and gui.messageBox(_("You are using an older version of Windows. This add-on requires {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
+	if sys.getwindowsversion().build < 15063 and gui.messageBox(_("You are using an older version of Windows. This add-on requires {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
 	# Translators: title of the dialog shown when attempting to install the add-on on an old version of Windows.
 	_("Old Windows version"), wx.YES | wx.NO | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION) == wx.NO:
 		raise RuntimeError("Old Windows version detected")
