7f07fc4983e178c86952b8b41842ea13a5a7b2c0
Joseph Lee <joseph.lee22590@gmail.com>
2017-12-06 19:25:38 -0800

    Mail: support newer versions only (hxoutlook.exe)

--- addon/appModules/hxoutlook.py
+++ addon/appModules/hxoutlook.py
@@ -6,4 +6,30 @@
 
 """An appModule for the Windows 10 Mail app"""
 
-from hxmail import *
+from nvdaBuiltin.appModules.hxoutlook import *
+import api
+from NVDAObjects.UIA import UIA
+from NVDAObjects.behaviors import RowWithFakeNavigation
+
+class MailItemRow(RowWithFakeNavigation, UIA):
+
+	def script_moveToPreviousColumn(self, gesture):
+		cur = api.getNavigatorObject()
+		if cur == self:
+			new = None
+		elif cur.parent != self:
+			new = self
+		else:
+			new = cur.previous
+		self._moveToColumn(new)
+	script_moveToPreviousColumn.canPropagate = True
+	script_moveToPreviousColumn.__doc__ = RowWithFakeNavigation.script_moveToPreviousColumn.__doc__
+
+
+class AppModule(AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self,obj,clsList):
+		if WordDocument in clsList:
+			clsList.insert(0,MailWordDocument)
+		elif isinstance(obj, UIA) and obj.UIAElement.cachedAutomationID == "MailItem":
+			clsList.insert(0, MailItemRow)
