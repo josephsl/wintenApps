49be9cc23492d6da43275b5132deaef295ed435b
Joseph Lee <joseph.lee22590@gmail.com>
2018-03-09 23:41:41 -0800

    Settings: just silence all volume progress bars.
    Silence all possible volume value progress bars. It might be revisited if things change.

--- addon/appModules/systemsettings.py
+++ addon/appModules/systemsettings.py
@@ -53,13 +53,12 @@ class AppModule(appModuleHandler.AppModule):
 
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
 		# In build 17035, Settings/System/Sound has been added, but has an anoying volume progress bar.
-		# Still not fixed in build 17115, now the automation iD is different.
-		if isinstance(obj, UIA) and obj.UIAElement.cachedClassName == "ProgressBar" and isinstance(obj.next, UIA) and obj.next.UIAElement.cachedAutomationID == "SystemSettings_Audio_Output_VolumeValue_BatterySaverThresholdValueTextBlock":
+		# Still not fixed in build 17115, now the volume slider is another element.
+		if isinstance(obj, UIA) and obj.UIAElement.cachedClassName == "ProgressBar" and isinstance(obj.next, UIA) and obj.next.UIAElement.cachedAutomationID.startswith("SystemSettings_Audio_Output_VolumeValue_"):
 			try:
 				clsList.remove(ProgressBar)
 			except ValueError:
 				pass
-
 	# Sometimes, the same text is announced, so consult this cache.
 	_nameChangeCache = ""
 
