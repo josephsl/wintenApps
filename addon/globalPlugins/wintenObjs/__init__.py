36e1da15d4442b67b0708013f83652e580794cee
Joseph Lee <joseph.lee22590@gmail.com>
2018-03-09 20:32:29 -0800

    UIA notification: do not announce background notifications.
    If background texts appear, just forget about the notification.

--- addon/globalPlugins/wintenObjs/__init__.py
+++ addon/globalPlugins/wintenObjs/__init__.py
@@ -231,13 +231,6 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			import tones
 			# For debugging purposes.
 			tones.beep(500, 100)
-<<<<<<< HEAD
-=======
-		# Some apps still fire both live region change and notification events, including Calculator.
-		if isinstance(obj, UIA) and not hasattr(obj, "event_UIA_notification"):
-			# As long as this add-on supports Version 1703, keep a notification blacklist handy.
-			# Also, do not announce notifications from background apps.
-			if obj.appModule.appName not in ("calculator",) or obj.appModule == api.getFocusObject().appModule:
-				ui.message(displayString)
->>>>>>> master
+		# Do not announce notifications from background apps.
+		if obj.appModule != api.getFocusObject().appModule: return
 		nextHandler()
