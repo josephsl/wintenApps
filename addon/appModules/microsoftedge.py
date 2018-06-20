fb57536094445f337b46b0a4aa8536f029522241
Joseph Lee <joseph.lee22590@gmail.com>
2018-06-19 21:55:05 -0700

    Edge: copyright updates to match that of NVDA Core.
    NVDA Core issue 8423: notifications from Edge main process is not handled simply because the focused element is part of the content process. A pull request for this issue was sent to NV Access in June 2018, thus matching the copyright header.

--- addon/appModules/microsoftedge.py
+++ addon/appModules/microsoftedge.py
@@ -1,6 +1,9 @@
 # MicrosoftEdge.py
-# Part of Windows 10 App Essentials collection
+#A part of NonVisual Desktop Access (NVDA)
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
 # Copyright 2016-2018 Joseph Lee, released under GPL
+# Additional: copyright (C) 2018 NV Access Limited, Joseph Lee
 
 # Core Edge support provided by NvDA Core (NVDAObjects/UIA package)
 # Provides additional enhancements.
@@ -30,6 +33,6 @@ class AppModule(appModuleHandler.AppModule):
 		nextHandler()
 
 	def event_UIA_notification(self, obj, nextHandler, displayString=None, activityId=None, **kwargs):
-		# Even though content process is focused, notifications are fired by main Edge process.
+		# NVDA Core issue 8423: even though content process is focused, notifications are fired by main Edge process.
 		# The base object will simply ignore this, so notifications must be announced here and no more.
 		ui.message(displayString)
