74f3f373b81be04701998d3319ad880c53cf3c8f
Joseph Lee <joseph.lee22590@gmail.com>
2018-06-22 19:30:27 -0700

    Settings/reset: announce dialog content changes.
    At least in Version 1803, whenever dialog content changes, header text fires name change event. Coerce this into announcing dilaog description so the entire screen can be announced.

---
+++ addon/appModules/systemsettingsadminflows.py
@@ -0,0 +1,18 @@
+# Windows 10 Settings/Reset This PC
+# Part of Windows 10 App Essentials collection
+# Copyright 2018 Joseph Lee, released under GPL
+
+# Specific to Reset This PC window in Settings.
+
+import appModuleHandler
+import ui
+from NVDAObjects.UIA import UIA
+
+class AppModule(appModuleHandler.AppModule):
+
+	def event_nameChange(self, obj, nextHandler):
+		if isinstance(obj, UIA) and obj.UIAElement.cachedAutomationID == "HeaderTextBlock":
+			# Not only header should be announced, dialog text should also be announced as well.
+			# This works properly if the parent object is recognized as a dialog.
+			ui.message(obj.parent.description)
+		nextHandler()
