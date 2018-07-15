647ddbd1d23516400dc2e3ec9ee4d75b45575778
Joseph Lee <joseph.lee22590@gmail.com>
2018-07-14 21:33:44 -0700

    People (modern Outlook.Contacts): recognize search suggestions.
    Apart fomr a different automation ID, Contacts search suggestions are suggestions. This then allows NVDA to announce top suggestions.

---
+++ addon/appModules/peopleapp.py
@@ -0,0 +1,16 @@
+# WinTenApps/peopleapp.py
+# Part of Windows 10 App Essentials collection
+# Copyright 2018 Joseph Lee, released under GPL.
+
+# Various workarounds for People app (modern version of Outlook Contacts).
+
+import appModuleHandler
+from NVDAObjects.UIA import UIA, SuggestionListItem
+
+class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if isinstance(obj, UIA):
+			# Apart from a different automation ID, contacts search suggestions are suggestions.
+			if obj.UIAElement.cachedAutomationID == "SuggestedListItem":
+				clsList.insert(0, SuggestionListItem)
