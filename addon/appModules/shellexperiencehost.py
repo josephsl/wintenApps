09c7b2629858eb4ed5e023170a79d5e81c9f057c
Joseph Lee <joseph.lee22590@gmail.com>
2018-07-27 21:06:43 -0700

    Shell Experience Host: remove tile submenu workaround.
    as the global plugin will take care of tile submenus, this workaround is no longer needed in Shell Experience Host. Also, majority of app module content will come from built-in version.

--- addon/appModules/shellexperiencehost.py
+++ addon/appModules/shellexperiencehost.py
@@ -4,37 +4,10 @@
 #See the file COPYING for more details.
 # Borrowed directly from NVDA Core (2016-2018 Joseph Lee)
 
-import appModuleHandler
-from NVDAObjects.IAccessible import IAccessible, ContentGenericClient
+from nvdaBuiltin.appModules.shellexperiencehost import *
 from NVDAObjects.UIA import UIA
-import controlTypes
 
-# Submenus in Start menu tiles context menu are not recognized as such (at least in WinTen Version 1511).
-# Also, make sure to get rid of position info announcement, as it's quite anoying.
-class TileSubMenu(UIA):
-
-	def _get_states(self):
-		# Borrowed from NVDA Core issue 5178 code (fixed provided by the same author).
-		states = super(TileSubMenu, self).states
-		states.add(controlTypes.STATE_HASPOPUP)
-		return states
-
-	def _get_positionInfo(self):
-		return {}
-
-class AppModule(appModuleHandler.AppModule):
-
-	def chooseNVDAObjectOverlayClasses(self,obj,clsList):
-		if isinstance(obj, IAccessible):
-			try:
-				# #5288: Never use ContentGenericClient, as this uses displayModel
-				# which will freeze if the process is suspended.
-				clsList.remove(ContentGenericClient)
-			except ValueError:
-				pass
-		elif isinstance(obj, UIA):
-			if obj.UIAElement.cachedClassName == "MenuFlyoutSubItem":
-				clsList.insert(0, TileSubMenu)
+class AppModule(AppModule):
 
 	def event_NVDAObject_init(self, obj):
 		if isinstance(obj, UIA):
