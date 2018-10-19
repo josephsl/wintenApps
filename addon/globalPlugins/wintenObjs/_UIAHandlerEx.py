783d6594aec4f726bdde0c12139b9ca32c486796
Joseph Lee <joseph.lee22590@gmail.com>
2018-10-19 14:39:59 -0700

    UIAHandlerEx: backport UIA event performance changes from NVDA 2018.4. Re #51.

--- addon/globalPlugins/wintenObjs/_UIAHandlerEx.py
+++ addon/globalPlugins/wintenObjs/_UIAHandlerEx.py
@@ -23,11 +23,39 @@ class UIAHandlerEx(UIAHandler):
 				isUIA8=True
 			except (COMError,WindowsError,NameError):
 				self.clientObject=CoCreateInstance(CUIAutomation._reg_clsid_,interface=IUIAutomation,clsctx=CLSCTX_INPROC_SERVER)
+			# #7345: Instruct UIA to never map MSAA winEvents to UIA propertyChange events.
+			# These events are not needed by NVDA, and they can cause the UI Automation client library to become unresponsive if an application firing winEvents has a slow message pump. 
+			pfm=self.clientObject.proxyFactoryMapping
+			for index in xrange(pfm.count):
+				e=pfm.getEntry(index)
+				for propertyID in UIAPropertyIdsToNVDAEventNames.keys():
+					# Check if this proxy has mapped any winEvents to the UIA propertyChange event for this property ID 
+					try:
+						oldWinEvents=e.getWinEventsForAutomationEvent(UIA_AutomationPropertyChangedEventId,propertyID)
+					except IndexError:
+						# comtypes does not seem to correctly handle a returned empty SAFEARRAY, raising IndexError
+						oldWinEvents=None
+					if oldWinEvents:
+						# As winEvents were mapped, replace them with an empty list
+						e.setWinEventsForAutomationEvent(UIA_AutomationPropertyChangedEventId,propertyID,[])
+						# Changes to an enty are not automatically picked up.
+						# Therefore remove the entry and re-insert it.
+						pfm.removeEntry(index)
+						pfm.insertEntry(index,e)
 			if isUIA8:
-				try:
-					self.clientObject=self.clientObject.QueryInterface(IUIAutomation5)
-				except COMError:
-					self.clientObject=self.clientObject.QueryInterface(IUIAutomation4)
+				# #8009: use appropriate interface based on highest supported interface.
+				# #8338: made easier by traversing interfaces supported on Windows 8 and later in reverse.
+				for interface in reversed(CUIAutomation8._com_interfaces_):
+					try:
+						self.clientObject=self.clientObject.QueryInterface(interface)
+						break
+					except COMError:
+						pass
+				# Windows 10 RS5 provides new performance features for UI Automation including event coalescing and connection recovery. 
+				# Enable all of these where available.
+				if isinstance(self.clientObject,IUIAutomation6):
+					self.clientObject.CoalesceEvents=CoalesceEventsOptions_Enabled
+					self.clientObject.ConnectionRecoveryBehavior=ConnectionRecoveryBehaviorOptions_Enabled
 			log.info("UIAutomation: %s"%self.clientObject.__class__.__mro__[1].__name__)
 			self.windowTreeWalker=self.clientObject.createTreeWalker(self.clientObject.CreateNotCondition(self.clientObject.CreatePropertyCondition(UIA_NativeWindowHandlePropertyId,0)))
 			self.windowCacheRequest=self.clientObject.CreateCacheRequest()
@@ -48,7 +76,9 @@ class UIAHandlerEx(UIAHandler):
 			self.clientObject.AddPropertyChangedEventHandler(self.rootElement,TreeScope_Subtree,self.baseCacheRequest,self,UIAPropertyIdsToNVDAEventNames.keys())
 			for x in UIAEventIdsToNVDAEventNames.iterkeys():  
 				self.clientObject.addAutomationEventHandler(x,self.rootElement,TreeScope_Subtree,self.baseCacheRequest,self)
-			self.clientObject.AddNotificationEventHandler(self.rootElement,TreeScope_Subtree,self.baseCacheRequest,self)
+			# #7984: add support for notification event (IUIAutomation5, part of Windows 10 build 16299 and later).
+			if isinstance(self.clientObject, IUIAutomation5):
+				self.clientObject.AddNotificationEventHandler(self.rootElement,TreeScope_Subtree,self.baseCacheRequest,self)
 		except Exception as e:
 			self.MTAThreadInitException=e
 		finally:
@@ -56,10 +86,3 @@ class UIAHandlerEx(UIAHandler):
 		self.MTAThreadStopEvent.wait()
 		self.clientObject.RemoveAllEventHandlers()
 
-	def IUIAutomationNotificationEventHandler_HandleNotificationEvent(self,sender,NotificationKind,NotificationProcessing,displayString,activityId):
-		if not self.MTAThreadInitEvent.isSet():
-			# UIAHandler hasn't finished initialising yet, so just ignore this event.
-			return
-		import NVDAObjects.UIA
-		obj=NVDAObjects.UIA.UIA(UIAElement=sender)
-		eventHandler.queueEvent("UIA_notification",obj, sender=sender, notificationKind=NotificationKind, notificationProcessing=NotificationProcessing, displayString=displayString, activityId=activityId)
