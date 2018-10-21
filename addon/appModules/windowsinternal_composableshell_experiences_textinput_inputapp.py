16c2a4ec6cd9b4ee46b6dbe0724e40adfae6d3c4
Joseph Lee <joseph.lee22590@gmail.com>
2018-10-21 10:29:52 -0700

    Emoji panel: select the appropriate emoji when emoji panel opens to People emoji group in 19H1.
    In build 18262 (19H1), it is possible for emoji panel to open to last viewed group (page) instead opening to most recently used page. With this in effect, when emoji panel opens to People group at startup, NVDA becomes verbose and skin tone modifier gets selected instead. Thus detect this situation and select the first people emoji.

--- addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -11,5 +11,44 @@ This is applicable on Windows 10 Fall Creators Update and later."""
 
 # The add-on version of this module will extend the one that comes with NVDA Core (2018.3 and later).
 
-
 from nvdaBuiltin.appModules.windowsinternal_composableshell_experiences_textinput_inputapp import *
+
+class AppModule(AppModule):
+
+	def event_UIA_elementSelected(self, obj, nextHandler):
+		# #7273: When this is fired on categories, the first emoji from the new category is selected but not announced.
+		# Therefore, move the navigator object to that item if possible.
+		# However, in recent builds, name change event is also fired.
+		# For consistent experience, report the new category first by traversing through controls.
+		# #8189: do not announce candidates list itself (not items), as this is repeated each time candidate items are selected.
+		if obj.UIAElement.cachedAutomationID == "CandidateList": return
+		speech.cancelSpeech()
+		# Sometimes, due to bad tree traversal or wrong item getting selected, something other than the selected item sees this event.
+		# Sometimes clipboard candidates list gets selected, so ask NvDA to descend one more level.
+		if obj.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemsList":
+			obj = obj.firstChild
+		# In build 18262, emoji panel may open to People group and skin tone modifier or the list housing them gets selected.
+		elif obj.UIAElement.cachedAutomationID == "SkinTonePanelModifier_ListView":
+			obj = obj.next
+		elif obj.parent.UIAElement.cachedAutomationID == "SkinTonePanelModifier_ListView":
+			obj = obj.parent.next
+		candidate = obj
+		parent = obj.parent
+		if obj.UIAElement.cachedClassName == "ListViewItem" and isinstance(parent, UIA) and parent.UIAElement.cachedAutomationID != "TEMPLATE_PART_ClipboardItemsList":
+			# The difference between emoji panel and suggestions list is absence of categories/emoji separation.
+			# Turns out automation ID for the container is different, observed in build 17666 when opening clipboard copy history.
+			candidate = obj.parent.previous
+			if candidate is not None:
+				# Emoji categories list.
+				ui.message(candidate.name)
+				obj = candidate.firstChild
+		if obj is not None:
+			api.setNavigatorObject(obj)
+			obj.reportFocus()
+			braille.handler.message(braille.getBrailleTextForProperties(name=obj.name, role=obj.role, positionInfo=obj.positionInfo))
+			# Cache selected item.
+			self._recentlySelected = obj.name
+		else:
+			# Translators: presented when there is no emoji when searching for one in Windows 10 Fall Creators Update and later.
+			ui.message(_("No emoji"))
+		nextHandler()
