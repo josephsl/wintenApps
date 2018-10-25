e995bd39d86b0dcf5c77dcf58ea2665175e71152
Joseph Lee <joseph.lee22590@gmail.com>
2018-10-25 15:39:21 -0700

    Emoji panel: catch possible attribute error when emoji panel is being closed.
    In build 18267, possibly because of IA2 to UIA bridge or something, attribute error is raised when emoji panel is closing. This also causes noticeable freeze for a few seconds (workaround is to reopen emoji panel and go to the desktop right afterwards). Thus catch attribute error exception and return from name change event.

--- addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -83,3 +83,26 @@ class AppModule(AppModule):
 		elif childAutomationID == "TEMPLATE_PART_ClipboardTitleBar":
 			self.event_UIA_elementSelected(obj.lastChild.previous, nextHandler)
 		nextHandler()
+
+	def event_nameChange(self, obj, nextHandler):
+		# #49: reported by a user: on some systems, touch keyboard keys keeps firing name change event.
+		# Argh, in build 17704, whenever skin tones are selected, name change is fired by emoji entries (GridViewItem).
+		if ((obj.UIAElement.cachedClassName in ("CRootKey", "GridViewItem"))
+		or (obj.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemsList")
+		# And no, emoji entries should not be announced here.
+		or (self._recentlySelected is not None and self._recentlySelected in obj.name)):
+			return
+		# The word "blank" is kept announced, so suppress this on build 17666 and later.
+		if winVersion.winVersion.build > 17134:
+			# In build 17672 and later, return immediatley when element selected event on clipboard item was fired just prior to this.
+			# In some cases, parent will be None, as seen when emoji panel is closed in build 18267.
+			try:
+				if obj.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemIndex" or obj.parent.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemsList": return
+			except AttributeError:
+				return
+			if not self._emojiPanelJustOpened or obj.UIAElement.cachedAutomationID != "TEMPLATE_PART_ExpressionGroupedFullView": speech.cancelSpeech()
+			self._emojiPanelJustOpened = False
+		# Don't forget to add "Microsoft Candidate UI" as something that should be suppressed.
+		if obj.UIAElement.cachedAutomationID not in ("TEMPLATE_PART_ExpressionFullViewItemsGrid", "TEMPLATE_PART_ClipboardItemIndex", "CandidateWindowControl"):
+			ui.message(obj.name)
+		nextHandler()
