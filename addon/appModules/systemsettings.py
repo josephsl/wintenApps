6b5ea899c6b257b798e8e1ef17c92685313c313e
Joseph Lee <joseph.lee22590@gmail.com>
2018-11-14 20:37:44 -0800

    Settings: add labels for links in build 18282.
    Once again, unlabeled controls are found: Settings/Update and Security/Windows Update/various links due to rearranged UI in build 18282/pause updates link. Thus add labels for them.

--- addon/appModules/systemsettings.py
+++ addon/appModules/systemsettings.py
@@ -49,6 +49,9 @@ class AppModule(appModuleHandler.AppModule):
 			# Search indexer radio buttons in build 18267 has no label, but the simple first child does.
 			if obj.name == "" and obj.role == controlTypes.ROLE_RADIOBUTTON and not obj.UIAElement.cachedAutomationID:
 				obj.name = obj.simpleFirstChild.name
+			# Due to changes in Windows Update UI in build 18282, various links ship with no label.
+			elif obj.name == "" and obj.role == controlTypes.ROLE_LINK:
+				obj.name = obj.children[0].name
 
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
 		# In build 17035, Settings/System/Sound has been added, but has an anoying volume progress bar.
