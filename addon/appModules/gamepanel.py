37fa6c6bc4388dd30088ce0905e0f2b102f752ab
Joseph Lee <joseph.lee22590@gmail.com>
2018-12-19 07:38:58 -0800

    1709 EOL: remove GamePanel app module.
    Game Panel app module was necessary in old days where Game Bar wasn't accessible apart from via name change event announcement. Thus remove this, as Version 1803 includes Game Bar natively (not a panel), which is very accessible.

--- addon/appModules/gamepanel.py
+++
@@ -1,9 +0,0 @@
-import appModuleHandler
-import tones
-import ui
-
-class AppModule(appModuleHandler.AppModule):
-
-	def event_nameChange(self, obj, nextHandler):
-		ui.message(obj.name)
-		nextHandler()
