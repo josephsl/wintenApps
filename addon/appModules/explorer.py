b31b905e2620fa81faab87970b257b2ea6e80d9c
Joseph Lee <joseph.lee22590@gmail.com>
2018-12-19 12:08:09 -0800

    Explorer: clarify that build 18305 introduces the new shell window for emoji panel container.

--- addon/appModules/explorer.py
+++ addon/appModules/explorer.py
@@ -16,7 +16,7 @@ import winVersion
 class AppModule(AppModule):
 
 	def isGoodUIAWindow(self,hwnd):
-		# In build 18290 and later, it is the shell that raises window open event for emoji panel (the below check is subject to change).
+		# In build 18305 and later, it is the shell that raises window open event for emoji panel (the below check is subject to change).
 		if winVersion.winVersion.build > 17763 and winUser.getClassName(hwnd) == "ApplicationFrameWindow":
 			return True
 		return False
