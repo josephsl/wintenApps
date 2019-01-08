6ecd3e1970a66b205cde5772233c2c37d5be8f10
Joseph Lee <joseph.lee22590@gmail.com>
2019-01-07 20:16:16 -0800

    Shell Experience host: update copyright header

--- addon/appModules/shellexperiencehost.py
+++ addon/appModules/shellexperiencehost.py
@@ -1,5 +1,5 @@
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2015-2018 NV Access Limited
+#Copyright (C) 2015-2019 NV Access Limited, Joseph Lee
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 # Borrowed directly from NVDA Core (2016-2018 Joseph Lee)
