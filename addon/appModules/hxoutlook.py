8e117de39ae9094b3dff525ac689e0fc5e604f39
Joseph Lee <joseph.lee22590@gmail.com>
2019-03-08 11:07:01 -0800

    Mail: catch COM error not caught by NVDA Core.
    NVDA Core issue 9341 (and potentially 9333): when looking for parent of Word document tree interceptor object, COM error is thrown, causing NVDA to go silent or play error tones continuously after closing Mail app. Thus catch this error from the add-on.

--- addon/appModules/hxoutlook.py
+++ addon/appModules/hxoutlook.py
@@ -7,6 +7,7 @@
 """An appModule for the Windows 10 Mail app"""
 
 from nvdaBuiltin.appModules.hxoutlook import *
+from comtypes import COMError
 import api
 from NVDAObjects.UIA import UIA
 from NVDAObjects.behaviors import RowWithFakeNavigation
@@ -30,6 +31,26 @@ class MailItemRow(RowWithFakeNavigation, UIA):
 		# Customized because there is no easy way to obtain column position info for this object yet.
 		# Therefore report that this is not supported yet.
 		ui.message(_("Cannot move between rows"))
+
+# NVDA Core issue 9341: temporary workaround for COM error exception found in NVDA.
+class MailWordDocument(WordDocument):
+
+	treeInterceptorClass=MailWordDocumentTreeInterceptor
+	def _get_shouldCreateTreeInterceptor(self):
+		# Locate the Reading pane in the ancestors
+		condition=UIAHandler.handler.clientObject.createPropertyCondition(UIAHandler.UIA_ClassNamePropertyId,"ReadingPaneModern")
+		walker=UIAHandler.handler.clientObject.createTreeWalker(condition)
+		try:
+			parent=walker.NormalizeElement(self.UIAElement)
+		except COMError:
+			parent=None
+		if not parent:
+			return False
+		# If we can find the message headers, then it is read-only and therefore needs browseMode
+		# An editable document contains draft headers instead
+		condition=UIAHandler.handler.clientObject.createPropertyCondition(UIAHandler.UIA_AutomationIdPropertyId,"MessageHeader")
+		header=parent.findFirst(UIAHandler.TreeScope_Descendants,condition)
+		return bool(header)
 
 
 class AppModule(AppModule):
