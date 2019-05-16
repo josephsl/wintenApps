956353e65af3db4c4d49e62edc2a6ccd46198c01
Joseph Lee <joseph.lee22590@gmail.com>
2019-05-16 11:21:43 -0700

    Emoji panel: remove NVDA Core 2019.1 work.
    NVDA 2019.1 includes modern keyboard improvements from this add-on except for detecting specific issues with clipboard paste (tip text not being shown0. Thus remove code that is part of NVDA Core.

--- addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -12,48 +12,8 @@ This is applicable on Windows 10 Fall Creators Update and later."""
 # The add-on version of this module will extend the one that comes with NVDA Core (2018.3 and later).
 
 from nvdaBuiltin.appModules.windowsinternal_composableshell_experiences_textinput_inputapp import *
-from .skipTranslation import translate
 
 class AppModule(AppModule):
-
-	def event_UIA_elementSelected(self, obj, nextHandler):
-		# #7273: When this is fired on categories, the first emoji from the new category is selected but not announced.
-		# Therefore, move the navigator object to that item if possible.
-		# However, in recent builds, name change event is also fired.
-		# For consistent experience, report the new category first by traversing through controls.
-		# #8189: do not announce candidates list itself (not items), as this is repeated each time candidate items are selected.
-		if obj.UIAElement.cachedAutomationID == "CandidateList": return
-		speech.cancelSpeech()
-		# Sometimes, due to bad tree traversal or wrong item getting selected, something other than the selected item sees this event.
-		# Sometimes clipboard candidates list gets selected, so ask NvDA to descend one more level.
-		if obj.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemsList":
-			obj = obj.firstChild
-		# In build 18262, emoji panel may open to People group and skin tone modifier or the list housing them gets selected.
-		elif obj.UIAElement.cachedAutomationID == "SkinTonePanelModifier_ListView":
-			obj = obj.next
-		elif obj.parent.UIAElement.cachedAutomationID == "SkinTonePanelModifier_ListView":
-			# But this will point to nothing if emoji search results are not people.
-			if obj.parent.next is not None: obj = obj.parent.next
-			else: obj = obj.parent.parent.firstChild
-		candidate = obj
-		if obj and obj.UIAElement.cachedClassName == "ListViewItem" and obj.parent and isinstance(obj.parent, UIA) and obj.parent.UIAElement.cachedAutomationID != "TEMPLATE_PART_ClipboardItemsList":
-			# The difference between emoji panel and suggestions list is absence of categories/emoji separation.
-			# Turns out automation ID for the container is different, observed in build 17666 when opening clipboard copy history.
-			candidate = obj.parent.previous
-			if candidate is not None:
-				# Emoji categories list.
-				ui.message(candidate.name)
-				obj = candidate.firstChild
-		if obj is not None:
-			api.setNavigatorObject(obj)
-			obj.reportFocus()
-			braille.handler.message(braille.getBrailleTextForProperties(name=obj.name, role=obj.role, positionInfo=obj.positionInfo))
-			# Cache selected item.
-			self._recentlySelected = obj.name
-		else:
-			# Message included in NVDA Core
-			ui.message(translate("No emoji"))
-		nextHandler()
 
 	def event_UIA_window_windowOpen(self, obj, nextHandler):
 		# Make sure to announce most recently used emoji first in post-1709 builds.
@@ -90,29 +50,4 @@ class AppModule(AppModule):
 			# Under some cases, clipboard tip text isn't shown on screen, causing clipboard history title to be announced instead of most recently copied item.
 			clipboardItemsIndex = -2 if obj.children[-2].UIAElement.cachedAutomationID != childAutomationID else -1
 			self.event_UIA_elementSelected(obj.children[clipboardItemsIndex], nextHandler)
-		nextHandler()
-
-	def event_nameChange(self, obj, nextHandler):
-		# #49: reported by a user: on some systems, touch keyboard keys keeps firing name change event.
-		# Argh, in build 17704, whenever skin tones are selected, name change is fired by emoji entries (GridViewItem).
-		if ((obj.UIAElement.cachedClassName in ("CRootKey", "GridViewItem"))
-		# Just ignore useless clipboard status.
-		# Also top emoji search result must be announced for better user experience.
-		or (obj.UIAElement.cachedAutomationID in ("TEMPLATE_PART_ClipboardItemsList", "TEMPLATE_PART_Search_TextBlock"))
-		# And no, emoji entries should not be announced here.
-		or (self._recentlySelected is not None and self._recentlySelected in obj.name)):
-			return
-		# The word "blank" is kept announced, so suppress this on build 17666 and later.
-		if winVersion.winVersion.build > 17134:
-			# In build 17672 and later, return immediatley when element selected event on clipboard item was fired just prior to this.
-			# In some cases, parent will be None, as seen when emoji panel is closed in build 18267.
-			try:
-				if obj.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemIndex" or obj.parent.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemsList": return
-			except AttributeError:
-				return
-			if not self._emojiPanelJustOpened or obj.UIAElement.cachedAutomationID != "TEMPLATE_PART_ExpressionGroupedFullView": speech.cancelSpeech()
-			self._emojiPanelJustOpened = False
-		# Don't forget to add "Microsoft Candidate UI" as something that should be suppressed.
-		if obj.UIAElement.cachedAutomationID not in ("TEMPLATE_PART_ExpressionFullViewItemsGrid", "TEMPLATE_PART_ClipboardItemIndex", "CandidateWindowControl"):
-			ui.message(obj.name)
 		nextHandler()
