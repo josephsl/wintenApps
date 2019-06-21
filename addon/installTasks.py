9bcffdbf604eb79e497c101ade0dd2a2f51c3ce4
Joseph Lee <joseph.lee22590@gmail.com>
2019-06-21 08:42:03 -0700

    Install tasks: use winVersion module for comparing builds.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -5,16 +5,16 @@
 # Mostly checks compatibility.
 # Routines are partly based on other add-ons, particularly Place Markers by Noelia Martinez (thanks add-on authors).
 
-import sys
 import gui
 import wx
+import winVersion
 import addonHandler
 addonHandler.initTranslation()
 
 def onInstall():
 	requiredVer = "Windows 10 Version 1803"
 	# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows (minSupportedVersion is the minimum version required for this add-on).
-	if sys.getwindowsversion().build < 17134 and gui.messageBox(_("You are using an older version of Windows. This add-on requires {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
+	if winVersion.winVersion.build < 17134 and gui.messageBox(_("You are using an older version of Windows. This add-on requires {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
 	# Translators: title of the dialog shown when attempting to install the add-on on an old version of Windows.
 	_("Old Windows version"), wx.YES | wx.NO | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION) == wx.NO:
 		raise RuntimeError("Old Windows version detected")
