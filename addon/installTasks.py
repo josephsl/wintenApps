283bdb2fbbf0ff50e1fe26fa22ddc58cdc2aa21b
Joseph Lee <joseph.lee22590@gmail.com>
2019-06-26 10:48:04 -0700

    Install tasks: use winVersion,isWin10 function.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -14,7 +14,7 @@ addonHandler.initTranslation()
 def onInstall():
 	requiredVer = "Windows 10 Version 1809"
 	# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows (minSupportedVersion is the minimum version required for this add-on).
-	if winVersion.winVersion.build < 17763 and gui.messageBox(_("You are using an older version of Windows. This add-on requires {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
+	if not winVersion.isWin10(version=1809) and gui.messageBox(_("You are using an older version of Windows. This add-on requires {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
 	# Translators: title of the dialog shown when attempting to install the add-on on an old version of Windows.
 	_("Old Windows version"), wx.YES | wx.NO | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION) == wx.NO:
 		raise RuntimeError("Old Windows version detected")
