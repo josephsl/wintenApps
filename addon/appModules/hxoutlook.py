bb47d19d0658b1a275ef869b7214e5bc72130fcf
Joseph Lee <joseph.lee22590@gmail.com>
2019-06-25 19:48:53 -0700

    Mail: update copyright years

--- addon/appModules/hxoutlook.py
+++ addon/appModules/hxoutlook.py
@@ -1,8 +1,8 @@
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2016 NV Access Limited
+#Copyright (C) 2016-2019 NV Access Limited, Joseph Lee
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
-# Extended in 2017 by Joseph Lee
+# Extended in 2017 and 2019 by Joseph Lee
 
 """An appModule for the Windows 10 Mail app"""
 
