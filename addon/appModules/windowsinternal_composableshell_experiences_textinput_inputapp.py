0eec95b2da42e8cab5365d6907bbac1435bb5834
Joseph Lee <joseph.lee22590@gmail.com>
2019-09-09 20:00:03 -0700

    Modern keyboard: basic support for modern CJK IME in 20H1.
    In 20H1 (at least build 18963 and later), in addition to executable renames, modern IME for Chinese, Japanese, and Korean is introduced with a brand new UIA automation ID for the candidate window. Thus add basic support for it (in case of certain languages, IME candidates must be spelled).

--- addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -15,6 +15,47 @@ from nvdaBuiltin.appModules.windowsinternal_composableshell_experiences_textinpu
 
 class AppModule(AppModule):
 
+	def event_UIA_elementSelected(self, obj, nextHandler):
+		# #7273: When this is fired on categories, the first emoji from the new category is selected but not announced.
+		# Therefore, move the navigator object to that item if possible.
+		# However, in recent builds, name change event is also fired.
+		# For consistent experience, report the new category first by traversing through controls.
+		# #8189: do not announce candidates list itself (not items), as this is repeated each time candidate items are selected.
+		if obj.UIAElement.cachedAutomationID == "CandidateList": return
+		speech.cancelSpeech()
+		# Sometimes, due to bad tree traversal or wrong item getting selected, something other than the selected item sees this event.
+		# Sometimes clipboard candidates list gets selected, so ask NvDA to descend one more level.
+		if obj.UIAElement.cachedAutomationID == "TEMPLATE_PART_ClipboardItemsList":
+			obj = obj.firstChild
+		# In build 18262, emoji panel may open to People group and skin tone modifier or the list housing them gets selected.
+		elif obj.UIAElement.cachedAutomationID == "SkinTonePanelModifier_ListView":
+			obj = obj.next
+		elif obj.parent.UIAElement.cachedAutomationID == "SkinTonePanelModifier_ListView":
+			# But this will point to nothing if emoji search results are not people.
+			if obj.parent.next is not None: obj = obj.parent.next
+			else: obj = obj.parent.parent.firstChild
+		candidate = obj
+		if obj and obj.UIAElement.cachedClassName == "ListViewItem" and obj.parent and isinstance(obj.parent, UIA) and obj.parent.UIAElement.cachedAutomationID != "TEMPLATE_PART_ClipboardItemsList":
+			# The difference between emoji panel and suggestions list is absence of categories/emoji separation.
+			# Turns out automation ID for the container is different, observed in build 17666 when opening clipboard copy history.
+			candidate = obj.parent.previous
+			if candidate is not None:
+				# Emoji categories list.
+				ui.message(candidate.name)
+				obj = candidate.firstChild
+		if obj is not None:
+			api.setNavigatorObject(obj)
+			# NvDA Core issue 10093: for Japanese IME, candidates must be spelled.
+			if obj.parent.UIAElement.cachedAutomationID == "IME_Candidate_Window": speech.speakSpelling(obj.name, useCharacterDescriptions=True)
+			else: obj.reportFocus()
+			braille.handler.message(braille.getBrailleTextForProperties(name=obj.name, role=obj.role, positionInfo=obj.positionInfo))
+			# Cache selected item.
+			self._recentlySelected = obj.name
+		else:
+			from . import skipTranslation
+			skipTranslation.translate("No emoji")
+		nextHandler()
+
 	def event_UIA_window_windowOpen(self, obj, nextHandler):
 		# Make sure to announce most recently used emoji first in post-1709 builds.
 		# Fake the announcement by locating 'most recently used" category and calling selected event on this.
@@ -32,7 +73,7 @@ class AppModule(AppModule):
 		# Handle hardware keyboard and CJK IME suggestions.
 		# Treat it the same as CJK composition list - don't announce this if candidate announcement setting is off.
 		# In fact, in 20H1, this is the CJK IME candidates window.
-		elif inputPanelAutomationID in ("CandidateWindowControl", "IME_Candidate_Window") and config.conf["inputComposition"]["autoReportAllCandidates"]:
+		elif inputPanelAutomationID in ("CandidateWindowControl", "IME_Candidate_Window", "IME_Prediction_Window") and config.conf["inputComposition"]["autoReportAllCandidates"]:
 			try:
 				self.event_UIA_elementSelected(inputPanel.firstChild.firstChild, nextHandler)
 			except AttributeError:
