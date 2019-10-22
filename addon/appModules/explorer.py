b86ca6b63e56afc711847c1ff4239df1b794faa3
Joseph Lee <joseph.lee22590@gmail.com>
2019-10-21 21:34:08 -0700

    Explorer/modern keyboard: remove ApplicationFrameWindow workaround from Explorer app module.
    NVDA 2019.3 will include ApplicationFrameWindow UIA object workaround, thus remove it from File Explorer app module.

--- addon/appModules/explorer.py
+++ addon/appModules/explorer.py
@@ -11,21 +11,3 @@ Provides workarounds for controls such as identifying Start button, notification
 # The add-on version of this module will extend the one that comes with NVDA Core.
 
 from nvdaBuiltin.appModules.explorer import *
-
-class AppModule(AppModule):
-
-	def isGoodUIAWindow(self,hwnd):
-		# In build 18305 and later, it is the shell that raises window open event for emoji panel (the below check is subject to change).
-		if winVersion.isWin10(version=1903) and winUser.getClassName(hwnd) == "ApplicationFrameWindow":
-			return True
-		return False
-
-	def event_UIA_window_windowOpen(self, obj, nextHandler):
-		# Send UIA window open event to input app window.
-		if isinstance(obj, UIA) and obj.UIAElement.cachedClassName == "ApplicationFrameWindow":
-			inputPanelWindow = obj.firstChild
-			# In build 18963, executable name for modern keyboard has changed, so support both executables.
-			if inputPanelWindow and inputPanelWindow.appModule.appName in ("windowsinternal_composableshell_experiences_textinput_inputapp", "textinputhost"):
-				eventHandler.executeEvent("UIA_window_windowOpen", inputPanelWindow)
-				return
-		nextHandler()
