b670f8d21f33d14052bcc82245e68e9d39de1fc4
Joseph Lee <joseph.lee22590@gmail.com>
2019-10-25 10:00:39 -0700

    Cortana Conversations: respond to UIA tree changes in 1.1910 beta.
    In 1.1910 beta (which showed up for slow ring somehow), UIA tree has changed due to a pop-up dialog. Thus respond to this change.

--- addon/appModules/cortana.py
+++ addon/appModules/cortana.py
@@ -12,13 +12,14 @@ class AppModule(appModuleHandler.AppModule):
 
 	# Sometimes, Cortana's textual response is announced twice.
 	_cortanaResponse = ""
-	
+
 	def event_UIA_notification(self, obj, nextHandler, displayString=None, **kwargs):
 		# For some reason Cortana fires this event whenever user types and answer is received.
 		# Results are displayed inside a list.
 		# Thus respond to both and see what should be announced.
 		if displayString != "Cortana message received.": return
-		responses = api.getForegroundObject().firstChild.firstChild.firstChild
+		# Version 1.1910 (beta) changed UIA tree for responses list.
+		responses = api.getForegroundObject().children[1].firstChild.firstChild
 		try:
 			cortanaResponse = responses.children[-2].name
 		except IndexError:
