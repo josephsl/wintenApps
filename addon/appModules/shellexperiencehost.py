56957b2f7c763fb1269dcb511d1ceb2623f3b884
Joseph Lee <joseph.lee22590@gmail.com>
2019-11-13 12:27:24 -0800

    Shell Experience Host/Action Center: remove app module.
    There were various workarounds hosted inside Action Center/Shell Experience Host app module, notably brightness button workaround for Version 1809 and labels for some unlabeled controls. As this add-on no longer supports Version 1809 (as of 2020) and since unlabeled contorls includes a label, remove this app module from this add-on.

--- addon/appModules/shellexperiencehost.py
+++
@@ -1,7 +0,0 @@
-#A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2015-2019 NV Access Limited, Joseph Lee
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
-# Borrowed directly from NVDA Core (2016-2018 Joseph Lee)
-
-from nvdaBuiltin.appModules.shellexperiencehost import *
