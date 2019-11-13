333f26052af419275f698df1e7dd67fab81a6ec1
Joseph Lee <joseph.lee22590@gmail.com>
2019-11-13 12:25:29 -0800

    Search App and Text Input Host: remove app modules.
    App modules for 20H1 apps, notably Search App (Windows Search) and Text Input Host (modern keyboard) are now included in NVDA 2019.3. Thus remove app modules for these from this add-on.

--- addon/appModules/searchapp.py
+++
@@ -1,9 +0,0 @@
-#A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2015 NV Access Limited
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
-# Extended by Joseph Lee (copyright 2019, released under GPL)
-
-# Renamed from SearchUI in build 18965 onwards.
-
-from .searchui import *
