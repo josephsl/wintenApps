f102266caeb285bbeb987dbc1d7963390b9b00b2
Joseph Lee <joseph.lee22590@gmail.com>
2019-11-16 16:09:03 -0800

    Edge/EdgeHTML: remove A11YAnnouncements alert workaround.
    In the old days, whenever alerts from YouTube and other sites were presented, it used 'A11YAnnouncements' automation ID. This may not hold true anymore due to changes to Edge and websites in general. Thus remove this workaround.

--- addon/appModules/microsoftedge.py
+++ addon/appModules/microsoftedge.py
@@ -10,24 +10,3 @@
 # Provides additional enhancements.
 
 from nvdaBuiltin.appModules.microsoftedge import *
-from NVDAObjects.UIA import UIA
-import controlTypes
-import ui
-
-class AppModule(AppModule):
-
-	def event_liveRegionChange(self, obj, nextHandler):
-		if isinstance(obj, UIA):
-			# Accessibility message alerts.
-			# For some messages, system alert event is raised along with this event, so return.
-			if obj.role == controlTypes.ROLE_ALERT and obj.UIAElement.cachedAutomationID == "a11y-announcements-message":
-				return
-		nextHandler()
-
-	# For message alerts, system alert and live region changed are fired at the same time.
-	def event_UIA_systemAlert(self, obj, nextHandler):
-		if isinstance(obj, UIA):
-			if obj.role == controlTypes.ROLE_ALERT and obj.UIAElement.cachedAutomationID == "a11y-announcements-message":
-				# Mick Curran says use last child in case a series of live texts are part of this control.
-				ui.message(obj.lastChild.name)
-		nextHandler()
