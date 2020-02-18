973cad7927d0edc8f1b9b589890b8197ea36866d
Joseph Lee <joseph.lee22590@gmail.com>
2020-02-17 23:36:08 -0800

    Add app module for CommsApps.
    In 2020, as part of Calendar preview changes, Mail and Calendar are integrated as one executable (commsapps.exe). Therefore introduce a dedicated app module that combines Mail and Calendar app module code.

---
+++ addon/appModules/commsapps.py
@@ -0,0 +1,15 @@
+# WinTenApps/commsapps.py
+# Part of Windows 10 App Essentials collection
+# Copyright 2020 Joseph Lee, released under GPL.
+
+# In 2020, Mail and Calendar became one app, sharing an executable named commsapps.exe.
+# This app module is based mostly on old Calendar app with workarounds from Mail included.
+
+from .hxoutlook import *
+
+class AppModule(AppModule):
+
+	def event_NVDAObject_init(self, obj):
+		# It is quite anoying to hear the same text for name and description, so forget the description.
+		if obj.name != "" and obj.description == obj.name:
+			obj.description = None
