d57a3f32b150bd45f5b83403400148f9ee1ce570
Joseph Lee <joseph.lee22590@gmail.com>
2020-07-06 00:53:28 -0700

    WWAHost: use focus mode for apps hostedby WWA Host by default.
    Aligning with NVDA Core: use focus mode by default when WWA Host app starts. This change comes directly from NVDA Core.

--- addon/appModules/wwahost.py
+++ addon/appModules/wwahost.py
@@ -3,13 +3,16 @@
 # See the file COPYING for more details.
 # Copyright (C) 2012 NV Access Limited
 
-"""App module host for Windows 8.x WinRT apps hosted by wwahost.exe.
+"""App module host for Windows 8.x and 10 apps hosted by wwahost.exe.
+In Windows 8, apps written in Javascript are executed inside WWAHost, including some WinRT apps.
+In Windows 10, progressive web apps (PWA) and friends are hosted inside this process.
+App modules wishing to support apps hosted inside this process must subclass the AppModule class.
 """
 
-# Extended in 2019 to add additional properties and fixes.
+# Extended in 2019 and 2020 to add additional properties and fixes.
 
-from nvdaBuiltin.appModules.wwahost import *
 import ctypes
+import appModuleHandler
 import winKernel
 
 
@@ -18,12 +21,20 @@ def getAppNameFromHost(processId):
 	# App modules for these are named after the hosted app name.
 	processHandle = winKernel.openProcess(winKernel.SYNCHRONIZE | winKernel.PROCESS_QUERY_INFORMATION, False, processId)
 	length = ctypes.c_uint()
-	buf = winKernel.kernel32.GetApplicationUserModelId(processHandle, ctypes.byref(length), None)
-	appModel = ctypes.create_unicode_buffer(buf)
+	winKernel.kernel32.GetApplicationUserModelId(processHandle, ctypes.byref(length), None)
+	appModel = ctypes.create_unicode_buffer(length.value)
 	winKernel.kernel32.GetApplicationUserModelId(processHandle, ctypes.byref(length), appModel)
 	winKernel.closeHandle(processHandle)
 	# Sometimes app model might be empty, so raise errors and fall back to wwahost.
 	if not appModel.value:
 		raise LookupError
+	# App model is shown as familyName!appName,
+	# and importing files with the exclamation point in the middle of the name isn't supported.
+	# Therefore return only the app name portion.
 	# Convert this into lowercase to make the file name consistent with other NVDA app modules.
 	return appModel.value.split("!")[-1].lower()
+
+class AppModule(appModuleHandler.AppModule):
+
+	# WWAHost app content is treated as part of an app, not a browse mode document.
+	disableBrowseModeByDefault = True
