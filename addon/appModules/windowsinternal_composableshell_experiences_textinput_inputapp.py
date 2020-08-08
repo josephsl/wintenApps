2ecb32c04f11534ed7911f524942175926dc8a3c
Joseph Lee <joseph.lee22590@gmail.com>
2020-08-08 10:07:00 -0700

    Modern keyboard: use isWin10 function when comparing build ranges in name change handler.

--- addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -172,7 +172,7 @@ class AppModule(AppModule):
 		or (self._recentlySelected is not None and self._recentlySelected in obj.name)):
 			return
 		# The word "blank" is kept announced, so suppress this on build 17666 and later.
-		if winVersion.winVersion.build > 17134:
+		if winVersion.isWin10(version=1809):
 			# In build 17672 and later, return immediatley when element selected event on clipboard item was fired just prior to this.
 			# In some cases, parent will be None, as seen when emoji panel is closed in build 18267.
 			try:
