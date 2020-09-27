1fe387565d62f56370ab5a66c0bfad7c81218ae9
Joseph Lee <joseph.lee22590@gmail.com>
2020-09-27 00:18:52 -0700

    Install tasks (Flake8 E501): split long lines.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -3,7 +3,8 @@
 
 # Provides needed routines during add-on installation and removal.
 # Mostly checks compatibility.
-# Routines are partly based on other add-ons, particularly Place Markers by Noelia Martinez (thanks add-on authors).
+# Routines are partly based on other add-ons,
+# particularly Place Markers by Noelia Martinez (thanks add-on authors).
 
 import addonHandler
 addonHandler.initTranslation()
@@ -15,7 +16,8 @@ def onInstall():
 	import winVersion
 	requiredVer = "Windows 10 Version 1909"
 	if not winVersion.isWin10(version=1909) and gui.messageBox(
-		# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows (minSupportedVersion is the minimum version required for this add-on).
+		# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows
+		# (minSupportedVersion is the minimum version required for this add-on).
 		_("You are using an older version of Windows. This add-on requires {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion=requiredVer),
 		# Translators: title of the dialog shown when attempting to install the add-on on an old version of Windows.
 		_("Old Windows version"), wx.YES | wx.NO | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION
