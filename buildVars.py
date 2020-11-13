d9578b81e9b12a77cbefe27708c5ea5907c7238d
Joseph Lee <joseph.lee22590@gmail.com>
2020-11-13 15:01:00 -0800

    Add-on template: incorporate latest update, including base language for readme

--- buildVars.py
+++ buildVars.py
@@ -67,3 +67,8 @@ i18nSources = pythonSources + ["buildVars.py"]
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
 excludedFiles = []
+
+# Base language for the NVDA add-on
+# If your add-on is written in a language other than english, modify this variable.
+# For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
+baseLanguage = "en"
