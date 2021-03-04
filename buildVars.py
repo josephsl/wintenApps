acdcdf0d777dfd5dc4ec1803348f61e8f3c069da
Joseph Lee <joseph.lee22590@gmail.com>
2021-03-03 18:04:46 -0800

    Build vars and SConstruct: apply latest add-on template changes.
    Changes include docs localization fix and Markdown extensions support.

--- buildVars.py
+++ buildVars.py
@@ -70,3 +70,10 @@ excludedFiles = []
 # If your add-on is written in a language other than english, modify this variable.
 # For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
 baseLanguage = "en"
+
+# Markdown extensions for add-on documentation
+# Most add-ons do not require additional Markdown extensions.
+# If you need to add support for markup such as tables, fill out the below list.
+# Extensions string must be of the form "markdown.extensions.extensionName"
+# e.g. "markdown.extensions.tables" to add tables.
+markdownExtensions = []
