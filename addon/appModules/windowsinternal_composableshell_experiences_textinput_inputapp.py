a737636e0e0a911b6af4819df0146db4fb4c5c94
Joseph Lee <joseph.lee22590@gmail.com>
2021-05-19 21:01:07 -0700

    Modern keyboard/emoji panel announcement: remove cateogry/emoji name announcement condition.
    Version 1709 and 1803 emoji panel raises element selected event for categories and emojis themselves, and to avoid repetitions, element selected event handler was told to return early if object name and recently selected text are the same. Later, as part of old OS removal process, the condition associated with this check removed OS version check. However this causes emojis to not be announced when Tab is pressed while meoji panel is open and selective UIA event registration is on. Therefore remove obj.name and recently selected item text check altogether, restoring emoji announcement and removing essentially dead code.

--- addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -89,9 +89,6 @@ class AppModule(AppModule):  # type: ignore[misc]  # NOQA: F405
 			or obj.parent.UIAAutomationId == "TEMPLATE_PART_Sets_ListView"
 			# Specifically to suppress skin tone modifiers from being announced after an emoji group was selected.
 			or self._symbolsGroupSelected
-			# In Version 1709 and 1803, both categories and items raise element selected event when category changes.
-			# No longer applicable but kept just in case this is seen in newer releases.
-			or obj.name == self._recentlySelected
 		):
 			return
 		speech.cancelSpeech()
