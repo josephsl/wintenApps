0af8cebeb9e5bc18d220600382c7f3a1ffa8e4f2
Joseph Lee <joseph.lee22590@gmail.com>
2021-06-16 11:04:13 -0700

    Settings/Reset this PC: unconditionally manipulate local event handler group.
    Inspired by NVDA Core's UIA object/gain focus event: unconditionally remove/add local event handler group elements. This means NVDA doesn't have to check selective UIA event registration flag each time the element in question is encountered (in this case, Reset This PC dilaog header). Note that this causes NotImplementedError to be thrown when removing elements from local event handler group.

--- addon/appModules/systemsettingsadminflows.py
+++ addon/appModules/systemsettingsadminflows.py
@@ -22,13 +22,17 @@ class AppModule(appModuleHandler.AppModule):
 		# With selective UIA event registration on, name change event from the header text will not be handled.
 		import config
 		import UIAHandler
-		if config.conf["UIA"]["selectiveEventRegistration"]:
-			# First child is a title bar, but simple first child is the actual header text.
-			# Therefore try using simple first child unless the situation changes in the future.
+		# First child is a title bar, but simple first child is the actual header text.
+		# Therefore try using simple first child unless the situation changes in the future.
+		# Wrap this inside a try/except block
+		# as NotImplementedError is raised if selective event registration is off.
+		try:
 			UIAHandler.handler.removeEventHandlerGroup(
 				obj.simpleFirstChild.UIAElement, UIAHandler.handler.localEventHandlerGroup
 			)
 			UIAHandler.handler.addEventHandlerGroup(
 				obj.simpleFirstChild.UIAElement, UIAHandler.handler.localEventHandlerGroup
 			)
+		except NotImplementedError:
+			pass
 		nextHandler()
