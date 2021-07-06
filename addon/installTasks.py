1cab63133a5c5884eb38fc2a181a68d9fd9b8877
Joseph Lee <joseph.lee22590@gmail.com>
2021-07-06 13:27:55 -0700

    Install tasks (optimization): return early if the system is not running Windows 10 or later.
    Optimization: there is no need to check feature updates and such if this is not even Windows 10. Therefore display the error dialog itself if running on older Windows releases.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -15,22 +15,26 @@ def onInstall():
 	import wx
 	import winVersion
 	currentWinVer = winVersion.getWinVer()
+	# Windows App Essentials requires Windows 10 or later.
+	# Return early by displaying an error dialog and raising runtime error.
+	if currentWinVer < winVersion.WIN10:
+		gui.messageBox(
+			# Translators: Dialog text shown when trying to install the add-on on releases earlier than Windows 10.
+			_("You are using an older version of Windows. This add-on requires Windows 10 or later."),
+			# Translators: title of the error dialog shown when trying to install the add-on in unsupported systems.
+			# Unsupported systems include Windows versions earlier than 10 and old Windows 10 feature updates.
+			_("Unsupported Windows release"), wx.OK | wx.ICON_ERROR
+		)
+		raise RuntimeError("Windows App Essentials requires Windows 10 or later")
 	# For now only check Windows 10.
 	windowsReleaseSeries = "Windows 10"
 	minimumSupportedRelease = winVersion.WIN10_20H2
 	minimumSupportedReleaseName = "Windows 10 20H2"
 	addonInstallPossible = True
 	addonInstallErrorMessage = ""
-	# Windows App Essentials requires Windows 10 or later.
-	if currentWinVer < winVersion.WIN10:
-		addonInstallErrorMessage = _(
-			# Translators: Dialog text shown when trying to install the add-on on releases earlier than Windows 10.
-			"You are using an older version of Windows. This add-on requires Windows 10 or later."
-		)
-		addonInstallPossible = False
 	# Windows App Essentials does not support old feature updates.
 	# This is the case for Windows 10 (to be expanded to Windows 11 in the future).
-	elif winVersion.WIN10 <= currentWinVer < minimumSupportedRelease:
+	if winVersion.WIN10 <= currentWinVer < minimumSupportedRelease:
 		addonInstallErrorMessage = _(
 			# Translators: Dialog text shown when trying to install the add-on on an unsupported Windows release.
 			# winRelease can be Windows 10, Windows 11, or other release series name.
