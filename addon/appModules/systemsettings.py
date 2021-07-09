b7cb48d101e3e53517152547beac6a59c4b5789c
Joseph Lee <joseph.lee22590@gmail.com>
2021-07-09 15:54:17 -0700

    Modern keyboard and Settings: typo fix.
    Reported by a user: attribute error is raised when trying to load Settings app module while using NVDA 2021.1 RC, traced to typos when trying to load refactored control types attributes.

--- addon/appModules/systemsettings.py
+++ addon/appModules/systemsettings.py
@@ -34,12 +34,12 @@ XAML_CLASS_ELEMENT_NAMES = [
 # Support control types refactor (both before (2021.1) and after (2021.2) for a time).
 if hasattr(controlTypes, "Role"):
 	ROLE_LINK = controlTypes.Role.LINK
-	ROLE_LISTITEM = controlTypes.ROLE_LISTITEM
-	ROLE_STATICTEXT = controlTypes.ROLE_STATICTEXT
-else:
-	ROLE_LINK = controlTypes.ROLE_LINK
 	ROLE_LISTITEM = controlTypes.Role.LISTITEM
 	ROLE_STATICTEXT = controlTypes.Role.STATICTEXT
+else:
+	ROLE_LINK = controlTypes.ROLE_LINK
+	ROLE_LISTITEM = controlTypes.ROLE_LISTITEM
+	ROLE_STATICTEXT = controlTypes.ROLE_STATICTEXT
 
 
 # App module class comes from built-in System Settings app module but Mypy doesn't know that.
