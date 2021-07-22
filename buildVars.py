a163cc63ba7087a309c132e926dd3374e71f6c1d
Joseph Lee <joseph.lee22590@gmail.com>
2021-07-21 23:49:04 -0700

    NVDA compatibility: last tested version -> 2021.2

--- buildVars.py
+++ buildVars.py
@@ -35,7 +35,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2020.4",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2021.1",
+	"addon_lastTestedNVDAVersion": "2021.2",
 	# Minimum Windows version supported
 	# Note: not to be included in the manifest, kept here to remind people about feature update support.
 	"addon_minimumWindowsVersion": "10.0.19042",
