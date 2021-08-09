80436f0a8234855d474cebf5e062db6345db19d9
Joseph Lee <joseph.lee22590@gmail.com>
2021-08-08 20:16:32 -0700

    Calculator: remove code that is integrated into NVDA 2021.2.
    Announcing expressions in braille and handling Delete key is now part of NVDA 2021.2.

--- addon/appModules/calculator.py
+++ addon/appModules/calculator.py
@@ -7,12 +7,7 @@
 
 # NVDA Core includes bulk of this app module.
 from nvdaBuiltin.appModules.calculator import *  # NOQA: F403
-import api
-import queueHandler
-import ui
 from NVDAObjects.UIA import UIA
-import braille
-import scriptHandler
 
 
 # Mypy should be reminded that this app module is powered by built-in Calculator app module.
@@ -32,36 +27,5 @@ class AppModule(AppModule):  # type: ignore[misc]  # NOQA: F405
 		if activityId == "GraphViewChanged" and self._resultsCache == displayString:
 			return
 		self._resultsCache = displayString
-		# NVDA Core issue 12268: for "DisplayUpdated", announce display strings in braille and move on.
-		if activityId == "DisplayUpdated":
-			braille.handler.message(displayString)
 		# Call the built-in app module version of UIA notification event handler.
 		super(AppModule, self).event_UIA_notification(obj, nextHandler, activityId=activityId, **kwargs)
-
-	# A list of native commands to handle calculator result announcement.
-	_calculatorResultGestures = (
-		"kb:enter",
-		"kb:numpadEnter",
-		"kb:escape",
-		"kb:delete",
-		"kb:numpadDelete"
-	)
-
-	@scriptHandler.script(gestures=_calculatorResultGestures)
-	def script_calculatorResult(self, gesture):
-		# To prevent double focus announcement, check where we are.
-		focus = api.getFocusObject()
-		gesture.send()
-		# In newer app releases, calculator result keeps firing name change event,
-		# so tell it to do so if and only if enter has been pressed.
-		self._shouldAnnounceResult = True
-		# Hack: only announce display text when an actual calculator button (usually equals button) is pressed.
-		# In newer app releases, pressing enter does not move focus to equals button.
-		if isinstance(focus, UIA):
-			if focus.UIAAutomationId in ("CalculatorResults", "CalculatorAlwaysOnTopResults"):
-				queueHandler.queueFunction(queueHandler.eventQueue, ui.message, focus.name)
-			else:
-				resultsScreen = api.getForegroundObject().children[1].lastChild
-				if isinstance(resultsScreen, UIA) and resultsScreen.UIAElement.cachedClassName == "LandmarkTarget":
-					# And no, do not allow focus to move.
-					queueHandler.queueFunction(queueHandler.eventQueue, ui.message, resultsScreen.firstChild.name)
