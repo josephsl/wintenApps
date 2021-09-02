9a5c0621c715a7ca4147fb7dc15106920c1f010b
Joseph Lee <joseph.lee22590@gmail.com>
2021-09-02 14:46:38 -0700

    Microsoft Store (Windows 11): library filter choice label is the name of the first child, not XAML class names.
    Microsoft Store 22108 (Windows 11) uses XAML class name for library filter list (WinStore.ViewModels.MyLibraryFilterChoice). Thankfully the first child name is the label for filter choice items. Therefore tell NVDA to look at the first child name until it is resolved by Microsoft.

--- addon/appModules/winstore_app.py
+++ addon/appModules/winstore_app.py
@@ -22,6 +22,10 @@ class AppModule(appModuleHandler.AppModule):
 				and obj.firstChild and obj.firstChild.UIAAutomationId == "InstallControl"
 			):
 				obj.name = obj.firstChild.firstChild.name
+			# Version 22108 (Windows 11) changes the UI and comes with a label issue for library filter choice list.
+			# It uses XAML class name and the actual label is the first child's name.
+			elif obj.name == "WinStore.ViewModels.MyLibraryFilterChoice":
+				obj.name = obj.firstChild.name
 
 	# just like Settings app, have a cache of download progress text handy.
 	_appInstallProgress: str = ""
