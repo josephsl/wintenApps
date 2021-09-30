e96d281d311578fa88f15fb84b2b39a6405df54d
Joseph Lee <joseph.lee22590@gmail.com>
2021-09-30 05:36:22 -0700

    Revert "WinAppObjs/dialogs: remove unused dialog class import (also resolves Flake8 F401)" Re #63
    This reverts commit be71c6c7548d4c18deeea818d2e16761283bc3e5 by importing now used dialog class.

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -9,7 +9,7 @@ from typing import Optional, Any
 from comtypes import COMError
 import globalPluginHandler
 import ui
-from NVDAObjects.UIA import UIA
+from NVDAObjects.UIA import UIA, Dialog
 # Temporary: test for suggestions list until NVDA 2021.3 requirement is in effect.
 import NVDAObjects.UIA
 import api
