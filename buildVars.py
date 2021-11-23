0c0e9d15623ce712da5893ad50bc04be54c595c9
Joseph Lee <joseph.lee22590@gmail.com>
2021-11-23 13:02:50 -0800

    BuildVars: minimum Windows version -> 10.0.19043

--- buildVars.py
+++ buildVars.py
@@ -38,7 +38,7 @@ addon_info = {
 	"addon_lastTestedNVDAVersion": "2021.3",
 	# Minimum Windows version supported
 	# Note: not to be included in the manifest, kept here to remind people about feature update support.
-	"addon_minimumWindowsVersion": "10.0.19042",
+	"addon_minimumWindowsVersion": "10.0.19043",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
