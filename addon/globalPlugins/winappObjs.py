b05106b995c8035e945aefc151affc3f215c6aac
Joseph Lee <joseph.lee22590@gmail.com>
2021-12-02 08:11:28 -0800

    WinApps: remove Unicode prefix for log output.
    As it is no longer needed, remove Unicode (u) prefix from event tracking log output.

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -174,7 +174,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			element = obj.UIAElement
 			info.append(f"Automation Id: {obj.UIAAutomationId}")
 			info.append(f"class name: {element.cachedClassName}")
-			log.debug(u"winapps: UIA {debuginfo}".format(debuginfo=", ".join(info)))
+			log.debug("winapps: UIA {debuginfo}".format(debuginfo=", ".join(info)))
 
 	# Events defined in NVDA.
 
