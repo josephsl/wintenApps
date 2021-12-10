62bcd2733c27f0f4bea02c7a386fe4b29f2f9104
Joseph Lee <joseph.lee22590@gmail.com>
2021-12-09 21:53:43 -0800

    Settings: remove name change event handler.
    It turns out Storage/Disk Cleanup announcements do not require coercing the resulting name change event handler into live region change event (likely a change in recent NVDA and/or Windows release). In fact, in recent Windows 10 releases (including Windows 11), custom name change event is not even required to announce Disk Cleanup progress (tested with the add-on disabled). As a result, name change event handler was deprecated in 2021, and now removed in this commit, making Settings app module a bit simpler (no more event handler conversion, no need to check live region change conditions from name change event handler).

--- addon/appModules/systemsettings.py
+++ addon/appModules/systemsettings.py
@@ -110,20 +110,5 @@ class AppModule(AppModule):  # type: ignore[misc]  # NOQA: F405
 			except AttributeError:
 				pass
 
-	def event_nameChange(self, obj, nextHandler):
-		# Duplicate events are filtered out in recent Windows 10 and NVDA releases.
-		# See older add-on releases for details.
-		if isinstance(obj, UIA):
-			# Storage/disk cleanup progress bar raises name change event.
-			if (
-				# Because "Purging:" is announced multiple times,
-				# coerce this to live region change event, which does handle this case.
-				obj.UIAAutomationId == "SystemSettings_StorageSense_TemporaryFiles_InstallationProgressBar"
-				# Announce result text for Storage Sense/cleanup.
-				or obj.UIAAutomationId.startswith("SystemSettings_StorageSense_SmartPolicy_ExecuteNow")
-			):
-				self.event_liveRegionChange(obj, nextHandler)
-		nextHandler()
-
 	def event_appModule_loseFocus(self):
 		self._nameChangeCache = ""
