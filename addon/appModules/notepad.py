f159ace22675b2bfa609ee09e9cf92b6dff24c00
Joseph Lee <joseph.lee22590@gmail.com>
2021-12-17 14:30:02 -0800

    Notepad: suppress text announcement hwen Enter key is pressed.
    Similar to Start menu search field: when Enter key is pressed, NVDA announces the text just enetered. Therefore tell NVDA to not announce line text when Enter key is pressed.

--- addon/appModules/notepad.py
+++ addon/appModules/notepad.py
@@ -6,9 +6,21 @@
 
 import appModuleHandler
 import api
+from NVDAObjects.UIA import UIA
+
+
+class NotepadDocument(UIA):
+
+	# Do not announce the entered text when Enter key is pressed, similar to Start menu search field.
+	announceNewLineText = False
 
 
 class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		# Specifically to suppress text announcement when Enter key is pressed.
+		if isinstance(obj, UIA) and obj.windowClassName == "RichEditD2DPT":
+			clsList.insert(0, NotepadDocument)
 
 	def _get_statusBar(self):
 		# Notepad 11 uses Windows 11 user interface, therefore status bar is harder to obtain.
