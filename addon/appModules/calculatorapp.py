9798efafb01f7c94c2680de4f3f9505af5868adb
Joseph Lee <joseph.lee22590@gmail.com>
2022-01-05 10:51:55 -0800

    Windows 11 Calculator: remove bulk of the app module and leave it as a stub.
    As Windows 10 Calculator includes changes from Windows 11 Calculator, there is no longer a need to include app module class for the newer app release. Therefore Windows 11 Calculator will be a stub (alias) that will import everything from Windows 10 Calculator. With this, changes to support newer Calculator app releases can be done by editing just one file.

--- addon/appModules/calculatorapp.py
+++ addon/appModules/calculatorapp.py
@@ -3,35 +3,5 @@
 # Copyright 2021-2022 Joseph Lee, released under GPL
 
 # This is similar to older Calculator app but was redesigned from ground up.
-# NVDA Core includes bulk of this app module.
-from nvdaBuiltin.appModules.calculator import *  # NOQA: F403
-
-
-# Mypy should be reminded that this app module is powered by built-in Calculator app module.
-# Inform Flake8 as well.
-class AppModule(AppModule):  # type: ignore[misc]  # NOQA: F405
-
-	def event_UIA_notification(self, obj, nextHandler, displayString=None, activityId=None, **kwargs):
-		# Some notification messages are repeated (most notable being graph view change notification).
-		if activityId == "GraphViewChanged" and self._resultsCache == displayString:
-			return
-		self._resultsCache = displayString
-		# Version 10.2109 changes the UI a bit, requiring custom event handler implementation.
-		# NVDA Core issue 12268: for "DisplayUpdated", announce display strings in braille.
-		if activityId == "DisplayUpdated":
-			braille.handler.message(displayString)  # NOQA: F405
-			# The actual display text and other controls live inside a toggle control window.
-			# Therefore move one more level down compared to older Calculator releases.
-			resultElement = api.getForegroundObject().children[1].lastChild.firstChild  # NOQA: F405
-			# Redesigned in 2019 due to introduction of "always on top" i.e. compact overlay mode.
-			if resultElement.UIAElement.cachedClassName != "LandmarkTarget":
-				resultElement = resultElement.parent.children[1]
-			# Display updated activity ID seen when entering calculations should be ignored
-			# as as it is redundant if speak typed characters is on.
-			if (
-				resultElement
-				and resultElement.firstChild
-				and resultElement.firstChild.UIAAutomationId in noCalculatorEntryAnnouncements  # NOQA: F405
-			):
-				return
-		nextHandler()
+# NVDA Core and Windows 10 Calculator from this add-on includes bulk of this app module.
+from .calculator import *  # NOQA
