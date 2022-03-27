7c749db3d628da205e3424f940ebf032482be0b1
Joseph Lee <joseph.lee22590@gmail.com>
2022-03-27 09:29:19 -0700

    Voice Access (Windows 11 Version 22H2): announce microphone status when Space or Enter is pressed.
    UIA notification event workaround: because NVDA cannot handle uIA notification event properly if running by itself (invalid window handle is returned), announce microphone status when toggled by pressing Space or Enter keys. This is done by defining an overlay class for microphone button.

--- addon/appModules/voiceaccess.py
+++ addon/appModules/voiceaccess.py
@@ -5,8 +5,19 @@
 # Support for Windows 11 Voice Access (Version 22H2).
 
 import appModuleHandler
+import scriptHandler
+import ui
 from NVDAObjects.UIA import UIA
 from NVDAObjects.behaviors import Dialog
+
+
+class MicrophoneButton(UIA):
+
+	@scriptHandler.script(gestures=["kb:space", "kb:enter", "kb:numpadEnter"])
+	def script_toggleMicrophone(self, gesture):
+		# Until NVDA gets a chance to handle UIA notifications when these commands are pressed...
+		gesture.send()
+		ui.message(self.name)
 
 
 class AppModule(appModuleHandler.AppModule):
@@ -15,3 +26,5 @@ class AppModule(appModuleHandler.AppModule):
 		if isinstance(obj, UIA):
 			if obj.UIAElement.cachedClassName == "Popup":
 				clsList.insert(0, Dialog)
+			elif obj.UIAAutomationId == "MicrophoneButtonControl":
+				clsList.insert(0, MicrophoneButton)
