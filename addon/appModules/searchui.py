e528a4055056634f8052aa98bf8461e5b224aab5
Joseph Lee <joseph.lee22590@gmail.com>
2022-04-10 12:03:06 -0700

    SearchUI (Mypy): add no-redef code to satisfy Mypy

--- addon/appModules/searchui.py
+++ addon/appModules/searchui.py
@@ -11,7 +11,8 @@ import controlTypes
 from NVDAObjects.UIA import UIA, SuggestionListItem
 
 
-class AppModule(AppModule):
+# Mypy should be reminded that this app module is powered by built-in SearchUI app module.
+class AppModule(AppModule):  # type: ignore[no-redef]
 
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
 		if isinstance(obj, UIA):
