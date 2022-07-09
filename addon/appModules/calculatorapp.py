a61abcf4a5b707cc9e0aa14875c88fba73ea81f6
Joseph Lee <joseph.lee22590@gmail.com>
2022-07-09 13:38:17 -0700

    Calculator (Windows 11): remove calculatorapp app module.
    NVDA 2022.1 includes Windows 11 Calculator app module, therefore remove it from the add-on.

--- addon/appModules/calculatorapp.py
+++
@@ -1,9 +0,0 @@
-# Windows 11 Calculator
-# Part of Windows App Essentials collection
-# Copyright 2021-2022 Joseph Lee, released under GPL
-
-# This is similar to older Calculator app but was redesigned from ground up.
-# NVDA Core and Windows 10 Calculator from this add-on includes bulk of this app module.
-# App module part of NVDA 2022.1.
-from .calculator import AppModule, noCalculatorEntryAnnouncements
-__all__ = ["AppModule", "noCalculatorEntryAnnouncements"]
