52656723bacf05a4e10790f9245fb5fedd743a55
Joseph Lee <joseph.lee22590@gmail.com>
2022-07-22 15:58:48 -0700

    Microsoft Solitaire Collection: version 4.13 and later improves accessibility by adding labels to cards (deprecate the app module).
    The whole point of Solitaire app module was adding labels to cards. This was the case in version 4.12 and earlier where UIA localized control type must be fetched. Version 4.13 and later improves accessibility by adding labels to cards (at least suits and ranks but not stacks). Therefore, deprecate the entire app module and schedule it for removal in a future add-on release. Until the app module is removed, detect old version by noting that 'unknown' controls (cards) have no names.

--- addon/appModules/solitaire.py
+++ addon/appModules/solitaire.py
@@ -3,6 +3,7 @@
 # Copyright 2020-2022 Joseph Lee, released under GPL
 
 # Support for Microsoft Solitaire Collection.
+# DEPRECATED: version 4.13 and later improves accessibility significantly by adding labels for cards.
 
 import appModuleHandler
 import controlTypes
@@ -12,5 +13,6 @@ class AppModule(appModuleHandler.AppModule):
 
 	def event_NVDAObject_init(self, obj):
 		# Cards are seen as "unknown" by NVDA, but thankfully localized control type serves as the label.
-		if obj.role == controlTypes.Role.UNKNOWN:
+		# In 4.13 and later, card suits and ranks are labeled but not the stacks.
+		if obj.role == controlTypes.Role.UNKNOWN and not obj.name:
 			obj.name = obj.UIAElement.currentLocalizedControlType
