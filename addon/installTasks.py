bfb89dc66ab0c1d58ae093f9b7455667f8c35ff6
Joseph Lee <joseph.lee22590@gmail.com>
2022-07-29 20:34:16 -0700

    Install tasks: further comment clarifications based on latest commits. Re #74.
    Rather than saying 'older feature updates', say 'unsupported feature updates' as builds list records supported feature update builds across Windows releases.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -30,13 +30,13 @@ def onInstall():
 				), unsupportedWindowsReleaseTitle, wx.OK | wx.ICON_ERROR
 			)
 		raise RuntimeError("Attempting to install Windows App Essentials on Windows releases earlier than 10")
-	# Windows App Essentials does not support old feature updates.
+	# Windows App Essentials does not support unsupported feature updates.
 	# Until Windows 10 21H1 (19043), checking build range was acceptable because there was one to one mapping
 	# between feature updates/milestones and builds across clients and servers such as 1809/17763 (RS5).
 	# But 21H2 changed this: Windows 10 (19044), Windows Server 2022 (20348), and Windows 11 (22000)
 	# As these releases come from vibranium (vb), iron (fe), and cobalt (co) branches, respectively.
 	# This continues in 22H2 with Windows 10 (19045) and Windows 11 (22621).
-	# For consistency, display builds list across Windows releases.
+	# Therefore, display supported builds list across Windows releases.
 	# For compatibility, assume minimum supported version is the one listed below unless this is Windows 11.
 	# Old NVDA versions do not include minimum Windows version defined.
 	# Note that the add-on does support NVDA releases with Windows 10, 11, and Server 2022 defined.
