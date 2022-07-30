b7302bb233d9c65be3a845b1ad82948bbb20fae1
Joseph Lee <joseph.lee22590@gmail.com>
2022-07-29 17:31:49 -0700

    Install tasks: add supported Windows 11 builds list, temporarily remove support for Insider Preview builds. Re #74.
    Add builds 22000 and 22621 to Windows 11 supported builds list. As this is a copy of Windows 10 builds list procedure, Windows Insider Preview builds above 22621 are marked as 'unsupported' - this will be resolved in the next commit. One group of Insider Preview builds are indeed unsupported as of this commit: 22000 < build < 22621.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -80,6 +80,19 @@ def onInstall():
 			"You are using an unsupported {windowsRelease} release. "
 			"Supported releases: {windowsReleasesList}."
 		).format(windowsRelease=windowsReleaseSeries, windowsReleasesList=", ".join(supportedBuilds.values()))
+	else:
+		supportedBuilds = {
+			22000: "21H2",
+			22621: "22H2"
+		}
+		addonInstallPossible = currentWinVer.build in supportedBuilds
+		unsupportedWindowsReleaseText = _(
+			# Translators: Dialog text shown when trying to install the add-on on an unsupported Windows 10 release.
+			# Unlike Windows 11, Windows 10 releases from 21H2 are checked using a list of public builds.
+			# For example, if 21H1 and 21H2 are supported, the text will list supported releases at the end.
+			"You are using an unsupported {windowsRelease} release. "
+			"Supported releases: {windowsReleasesList}."
+		).format(windowsRelease=windowsReleaseSeries, windowsReleasesList=", ".join(supportedBuilds.values()))
 	if not addonInstallPossible:
 		if not globalVars.appArgs.minimal:
 			gui.messageBox(unsupportedWindowsReleaseText, unsupportedWindowsReleaseTitle, wx.OK | wx.ICON_ERROR)
