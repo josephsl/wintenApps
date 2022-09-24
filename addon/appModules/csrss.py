2870b42eaba57bcb5362d89fac9c36f572d870a6
Joseph Lee <joseph.lee22590@gmail.com>
2022-09-23 17:47:00 -0700

    CSRSS: handle virtual desktop switch announcement. Re #77.
    Copy name change event handler from WinAppObjs global plugin to CSRSS (client/server runtime subsystem) app module as virtual desktop switch announcements are raised by this new app module.

---
+++ addon/appModules/csrss.py
@@ -0,0 +1,19 @@
+# Client/Server Runtime Subsystem (Windows subsystem process)
+# Copyright 2022 Joseph Lee, released under GPL.
+
+# Specifically designed to handle virtual desktop switches.
+# Split from WinAppObjs global plugin in 2022.
+
+import appModuleHandler
+import ui
+
+
+class AppModule(appModuleHandler.AppModule):
+
+	def event_nameChange(self, obj, nextHandler):
+		# NVDA Core issue 5641: try catching virtual desktop switch event,
+		# which will result in name change for the desktop object.
+		if obj.windowClassName == "#32769":
+			import core
+			core.callLater(250, ui.message, obj.name)
+		nextHandler()
