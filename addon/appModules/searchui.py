7d862476cd046d4e6c430698c19894d657e87314
Joseph Lee <joseph.lee22590@gmail.com>
2022-10-01 12:50:15 -0700

    SearchUI/search field: use description getter to obtain search highlights text.
    Instead of a focus event, extend NVDA Core's Start search field class by adding a description getter which returns search highlights text if the last child element is present. This allows NVDA to announce search field label correctly if highlights is shown or not while not searching and searching, respectively (mostly affects speech output). As part of this commit, reintroduce overlay classes chooser and tlel it to recognized the enhanced Start search field and move to NVDA Core version, as well as transfer gain focus event from the app module to the description getter in the search field class and edit imports.

--- addon/appModules/searchui.py
+++ addon/appModules/searchui.py
@@ -7,18 +7,24 @@
 
 # Start menu search field is not used here but must be imported as it causes error when the app module loads.
 from nvdaBuiltin.appModules.searchui import AppModule, StartMenuSearchField  # NOQA: F401
-import controlTypes
-from NVDAObjects.UIA import UIA, SuggestionListItem
-import ui
-import queueHandler
+from NVDAObjects.UIA import UIA
+
+
+class StartMenuSearchField(StartMenuSearchField):
+
+	def _get_description(self):
+		# Detect search highlights and anounce it.
+		if self.lastChild.UIAAutomationId == "PlaceholderTextContentPresenter":
+			return self.lastChild.name
+		return super().description
 
 
 # Mypy should be reminded that this app module is powered by built-in SearchUI app module.
 class AppModule(AppModule):  # type: ignore[no-redef]
 
-	def event_gainFocus(self, obj, nextHandler):
-		if isinstance(obj, StartMenuSearchField):
-			# Detect search highlights and anounce it.
-			if obj.lastChild.UIAAutomationId == "PlaceholderTextContentPresenter":
-				queueHandler.queueFunction(queueHandler.eventQueue, ui.message, obj.lastChild.name)
-		nextHandler()
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if isinstance(obj, UIA):
+			if obj.UIAAutomationId == "SearchTextBox":
+				clsList.insert(0, StartMenuSearchField)
+				return
+		super().chooseNVDAObjectOverlayClasses(obj, clsList)
