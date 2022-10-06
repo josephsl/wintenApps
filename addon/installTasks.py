1621e7fe8c93e3ba22a993e9479538d28d71532a
Joseph Lee <joseph.lee22590@gmail.com>
2022-10-06 12:32:40 -0700

    Install tasks (Flake8 E501): use str.join to present a list of supported Windows releases. Re #78.
    Define supported releases inside a list so they can be joined. Unless the supported releases map is redesigned, this is the method to be used to list supported Windows releases.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -19,7 +19,14 @@ def onInstall():
 	currentWinVer = winVersion.getWinVer()
 	# #78: test the old and new (combined) install error messages, initially set to "False" for compatibility.
 	presentCombinedErrorMessage = False
-	windowsReleasesList = "Windows 10 21H2 (19044), 22H2 (19045), Windows Server 2022 (20348), Windows 11 21H2 (22000), 22H2 (22621), Windows Insider Preview"
+	windowsReleasesList = ", ".join([
+		"Windows 10 21H2 (19044)",
+		"22H2 (19045)",
+		"Windows Server 2022 (20348)",
+		"Windows 11 21H2 (22000)",
+		"22H2 (22621)",
+		"Windows Insider Preview"
+	])
 	unsupportedWindowsReleaseText = _(
 		# Translators: Dialog text shown when trying to install the add-on on an unsupported Windows release.
 		# Release name and build refer to Windows release in use (example: Windows 10 21H2 (19044)).
