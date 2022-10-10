2dc93f14621834955376087bd3d970a4b419effe
Joseph Lee <joseph.lee22590@gmail.com>
2022-10-09 21:46:17 -0700

    Install tasks: remove new/old message presentation test flag. Re #78

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -60,8 +60,6 @@ def onInstall():
 		or (windowsReleaseSeries == "Windows 11" and currentWinVer.build >= max(supportedBuilds))  # Insider Preview
 	):
 		return
-	# #78: test the old and new (combined) install error messages, initially set to "False" for compatibility.
-	presentCombinedErrorMessage = True
 	windowsReleasesListText = ", ".join([
 		"Windows 10 21H2 (19044)",
 		"22H2 (19045)",
