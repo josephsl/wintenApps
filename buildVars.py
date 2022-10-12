f3201861089d6461240fdaa02cfd098b23513986
Joseph Lee <joseph.lee22590@gmail.com>
2022-10-11 23:31:17 -0700

    BuildVars: define add-on source URL, license, and license URL.
    Add source URL (repo address), license (GPL 2), and license URL to build vars map, used when generating add-on store json file.

--- buildVars.py
+++ buildVars.py
@@ -31,7 +31,7 @@ addon_info = {
 	# URL for the add-on documentation support
 	"addon_url": "https://addons.nvda-project.org/",
 	# URL for the add-on repository where the source code can be found
-	"addon_sourceURL": None,
+	"addon_sourceURL": "https://github.com/josephsl/wintenApps",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
@@ -43,9 +43,9 @@ addon_info = {
 	# Do not change unless you know what you are doing!
 	"addon_updateChannel": None,
 	# Add-on license such as GPL 2
-	"addon_license": None,
+	"addon_license": "GPL v2",
 	# URL for the license document the ad-on is licensed under
-	"addon_licenseURL": None,
+	"addon_licenseURL": "https://www.gnu.org/licenses/gpl-2.0.html",
 }
 
 
