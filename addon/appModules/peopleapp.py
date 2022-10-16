40c2ec3e35a4e42ff5ec35bd5ff40dae0f163632
Joseph Lee <joseph.lee22590@gmail.com>
2022-10-15 17:26:50 -0700

    People: recognize contacts search field.
    Recognize edit field with the Automation Id of 'ContactSearchTextBox' as a UIA search field, allowing suggestion sounds to be played.

--- addon/appModules/peopleapp.py
+++ addon/appModules/peopleapp.py
@@ -5,13 +5,16 @@
 # Various workarounds for People app (modern version of Outlook Contacts).
 
 import appModuleHandler
-from NVDAObjects.UIA import UIA, SuggestionListItem
+from NVDAObjects.UIA import UIA, SuggestionListItem, SearchField
 
 
 class AppModule(appModuleHandler.AppModule):
 
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
 		if isinstance(obj, UIA):
-			# Apart from a different Automation Id, contacts search suggestions are suggestions.
+			# Apart from different Automation Id's,
+			# contacts search field and suggestions are search field and suggestions, respectively.
 			if obj.UIAAutomationId == "SuggestedListItem":
 				clsList.insert(0, SuggestionListItem)
+			elif obj.UIAAutomationId == "ContactSearchTextBox":
+				clsList.insert(0, SearchField)
