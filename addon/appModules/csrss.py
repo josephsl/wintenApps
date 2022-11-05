a8914efcf55b124dd47bb296916163d99c1064d3
Joseph Lee <joseph.lee22590@gmail.com>
2022-11-05 15:56:02 -0700

    CSRSS: increase virtual desktop switch announcements sensitivity.
    Incrase virtual desktop switch announcement sensitivity from quarter of a second (250 ms) to a tenth of a second (100 ms). This may cause virtual desktop switches to be announced a bit earlier, especially when pressing Control+Windows+left/right arrows to move between virtual desktops.

--- addon/appModules/csrss.py
+++ addon/appModules/csrss.py
@@ -15,5 +15,5 @@ class AppModule(appModuleHandler.AppModule):
 		# NVDA Core issue 5641: try catching virtual desktop switch event,
 		# which will result in name change for the desktop object.
 		if obj.windowClassName == "#32769":
-			core.callLater(250, ui.message, obj.name)
+			core.callLater(100, ui.message, obj.name)
 		nextHandler()
