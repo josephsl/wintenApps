1d59a0e61638c57f2911fe21f9f944f391ca062a
Joseph Lee <joseph.lee22590@gmail.com>
2022-12-13 23:51:57 -0800

    WinApps/Windows 11 shell: reclassify Windows 11 22H2 Moment 2 systray overflow window as a UIA element. Re #82.
    Windows 11 22H2 Moment 2 introduces a redesigned taskbar and system tray. However the new systray overflow cannot be interacted with via touch and mouse, caused by the window being IAccessible. Therefore reclassify the overflow window (class name: TopLevelWindowForOverflowXamlIsland) as UIA element so mouse and touch interaction can work by treating the window class name as good UIA window class name. This is simpler than patching File Explorer but the solution is editing the app module (to be done once Moment 2 is released).

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -50,6 +50,13 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		import winVersion
 		if winVersion.getWinVer().build in (22622, 22623):
 			log.info("winapps: Windows 11 22H2 beta detected")
+		# #82: Windows 11 22H2 Moment 2 introduces a redesigned taskbar and systray powered by Win32.
+		# However touch and mouse interaction does not work when systray overflow window is open.
+		# Therefore reclassify the new systray overflow window class name as a good UIA window class.
+		# A better solution is patching File Explorer app module but the below workaround will suffice.
+		goodUIAWindowClassNames = list(UIAHandler.goodUIAWindowClassNames)
+		goodUIAWindowClassNames.append("TopLevelWindowForOverflowXamlIsland")
+		UIAHandler.goodUIAWindowClassNames = tuple(goodUIAWindowClassNames)
 		# Try adding additional events and properties in the constructor.
 		# If it fails, try again after NVDA is fully initialized.
 		# #81: but only do all this if additional events and properties are populated.
