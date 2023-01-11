2ccac9e6dfa2a5dac3c057d08e9a1895773c441a
Joseph Lee <joseph.lee22590@gmail.com>
2023-01-11 01:27:42 -0800

    WinApps/system tray redesign: limit good UIA class name manipulation to Windows 11 22H2 and later.
    Redesigned system tray isn't available on Windows releases earlier than 11 22H2 (beta at the moment), therefore limit the good UIA class name workarond to 11 22H2 and later.

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -74,9 +74,10 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		# However touch and mouse interaction does not work when systray overflow window is open.
 		# Therefore reclassify the new systray overflow window class name as a good UIA window class.
 		# A better solution is patching File Explorer app module but the below workaround will suffice.
-		goodUIAWindowClassNames = list(UIAHandler.goodUIAWindowClassNames)
-		goodUIAWindowClassNames.append("TopLevelWindowForOverflowXamlIsland")
-		UIAHandler.goodUIAWindowClassNames = tuple(goodUIAWindowClassNames)
+		if winVersion.getWinVer() >= winVersion.WIN11_22H2:
+			goodUIAWindowClassNames = list(UIAHandler.goodUIAWindowClassNames)
+			goodUIAWindowClassNames.append("TopLevelWindowForOverflowXamlIsland")
+			UIAHandler.goodUIAWindowClassNames = tuple(goodUIAWindowClassNames)
 
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
 		if not isinstance(obj, UIA):
