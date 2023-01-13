f9859f2c92817cef9dc459e61d1f5d3a5f4e9fba
Joseph Lee <joseph.lee22590@gmail.com>
2023-01-13 07:54:16 -0800

    Open With: add 'Open With' window as a good UIA window.
    Edit 'isGoodUIAWindow' method by treating 'Open With' class as a good UIA window (the rest of the app module is not needed, similar to old ad-don releases which had this pap module in the past).

--- addon/appModules/openwith.py
+++ addon/appModules/openwith.py
@@ -1,48 +1,21 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2011-2022 NV Access Limited, Joseph Lee
+# Copyright (C) 2011-2023 NV Access Limited, Joseph Lee
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
 
-import appModuleHandler
-import controlTypes
-from NVDAObjects.UIA import UIA
-from NVDAObjects.behaviors import Dialog
+from nvdaBuiltin.appModules.openwith import AppModule
 import winUser
 
-#win8hack: the nondefault items in the list of applications are not labeled
-class NonDefaultAppTile(UIA):
 
-	def _get_name(self):
-		firstChild=self.firstChild
-		if firstChild:
-			next=firstChild.next
-			if next:
-				return next.name
-		return super(NonDefaultAppTile,self).name
-
-class ImmersiveOpenWithFlyout(Dialog,UIA):
-
-	role=controlTypes.Role.DIALOG
-
-	#win8hack: This window never actually gets the physical focus thus tabbing etc goes to the original window
-	#So Force it to get focus
-	def event_focusEntered(self):
-		self.setFocus()
-		super(ImmersiveOpenWithFlyout,self).event_focusEntered()
-
-class AppModule(appModuleHandler.AppModule):
-
-	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-		if isinstance(obj, UIA):
-			automationId = obj.UIAAutomationId
-			if automationId == "NonDefaultAppTile":
-				clsList.insert(0, NonDefaultAppTile)
-			elif automationId == "ImmersiveOpenWithFlyout":
-				clsList.insert(0, ImmersiveOpenWithFlyout)
+class AppModule(AppModule):
 
 	def isGoodUIAWindow(self, hwnd):
-		# #11335: Open With dialog isn't read in Windows 10 Version 2004 (May 2020 Update).
+		# NVDA Core issue 11335: Open With dialog isn't read in Windows 10 Version 2004 (May 2020 Update).
 		# Note that treating the below window as a UIA window will make NVDA no longer announce "pane".
-		if winUser.getClassName(hwnd) == "Shell_Flyout":
+		# Add support for Windows 11 Version 22H2 as well.
+		if winUser.getClassName(hwnd) in (
+			"Shell_Flyout",  # Windows 10
+			"Open With",  # Windows 11 22H2
+		):
 			return True
 		return False
