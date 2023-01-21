76d8436f393b673f9d3c5094ea093203ddace4a9
Joseph Lee <joseph.lee22590@gmail.com>
2023-01-21 08:16:49 -0800

    WinApps: remove taskbar item class

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -15,54 +15,6 @@ import addonHandler
 addonHandler.initTranslation()
 
 
-# Ideally this should be part of File Explorer app module but to avoid conflicts with other add-ons...
-class TaskbarItem(UIA):
-
-	def _get_itemName(self):
-		# Icon name contains open window count if windows are open after a hyphen (-).
-		return self.name.rpartition(" - ")[0] if " -" in self.name else self.name
-
-	def _get_positionInfo(self):
-		# Position info is included in build 25281.
-		positionInfo = super().positionInfo
-		if not positionInfo:
-			taskbarItems = [item for item in self.parent.children if isinstance(item, TaskbarItem)]
-			positionInfo = {
-				"indexInGroup": taskbarItems.index(self) + 1, "similarItemsInGroup": len(taskbarItems)
-			}
-		return positionInfo
-
-	def announceDragPosition(self):
-		import ui
-		left = self.previous if isinstance(self.previous, TaskbarItem) else None
-		right = self.next if isinstance(self.next, TaskbarItem) else None
-		if left and right:
-			# Translators: announced when rearranging taskbar icons in Windows 11.
-			ui.message(_("{appName} moved between {previousAppName} and {nextAppName}").format(
-				appName=self.itemName, previousAppName=left.itemName, nextAppName=right.itemName
-			))
-		elif left and not right:
-			# Translators: announced when rearranging taskbar icons in Windows 11.
-			ui.message(_("{appName} moved to end of the apps list").format(appName=self.itemName))
-		elif right and not left:
-			# Translators: announced when rearranging taskbar icons in Windows 11.
-			ui.message(_("{appName} moved to beginning of the apps list").format(appName=self.itemName))
-
-	@scriptHandler.script(gesture="kb:alt+shift+rightArrow")
-	def script_moveRight(self, gesture):
-		announcePosition = isinstance(self.next, TaskbarItem)
-		gesture.send()
-		if announcePosition:
-			wx.CallAfter(self.announceDragPosition)
-
-	@scriptHandler.script(gesture="kb:alt+shift+leftArrow")
-	def script_moveLeft(self, gesture):
-		announcePosition = isinstance(self.previous, TaskbarItem)
-		gesture.send()
-		if announcePosition:
-			wx.CallAfter(self.announceDragPosition)
-
-
 # #20: don't even think about proceeding in secure screens.
 def disableInSecureMode(cls):
 	return globalPluginHandler.GlobalPlugin if globalVars.appArgs.secure else cls
