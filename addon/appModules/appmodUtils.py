3a047a0da9d842d907a1c396730083f95e7edd9d
Joseph Lee <joseph.lee22590@gmail.com>
2023-03-04 17:45:01 -0800

    App module utilities: remove unused imports and ui.message call. Re #85.
    The job of the function (name comes from File Explorer app module) is to return the UIA element, not to announce what it is.

--- addon/appModules/appmodUtils.py
+++ addon/appModules/appmodUtils.py
@@ -4,17 +4,12 @@
 
 # Adds routines used by various app modules.
 
-from typing import Callable, Optional
-import appModuleHandler
-import api
-import ui
 import UIAHandler
 from NVDAObjects.UIA import UIA
 from NVDAObjects import NVDAObject
 
 
 def _detectEmptyFolder(self, obj: NVDAObject):
-	import UIAHandler
 	from comtypes import COMError
 	clientObject = UIAHandler.handler.clientObject
 	condition = clientObject.createPropertyCondition(UIAHandler.UIA_ClassNamePropertyId, "UIItemsView")
@@ -25,11 +20,4 @@ def _detectEmptyFolder(self, obj: NVDAObject):
 		element = element.buildUpdatedCache(UIAHandler.handler.baseCacheRequest)
 	except (ValueError, COMError):
 		return
-	lastUIItem = UIA(UIAElement=element).lastChild
-	# NVDA Core issue 5759: announce empty folder text.
-	if (
-		isinstance(lastUIItem, UIA)
-		and lastUIItem.role == controlTypes.Role.STATICTEXT
-		and lastUIItem.UIAElement.currentClassName == "Element"
-	):
-		ui.message(lastUIItem.name)
+	return UIA(UIAElement=element)
