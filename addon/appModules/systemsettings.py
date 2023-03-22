b12cd421459ce4a74bcb14f6abd4459ee69ba0fa
Joseph Lee <joseph.lee22590@gmail.com>
2023-03-22 08:34:07 -0700

    Settings/taskbar/search combo box: do not add label if the combo box's parent is search grouping item.
    Taskbar/search combo box label issue is resolved through a search grouping. Therefore detect this grouping (parent of the combo box element) and do not add search label accordingly. This is the case with build 22621.1483 (release preview), with the workaround to be removed once this fix comes to general availability channel.

--- addon/appModules/systemsettings.py
+++ addon/appModules/systemsettings.py
@@ -73,11 +73,12 @@ class AppModule(AppModule):  # type: ignore[no-redef]
 			elif obj.UIAElement.cachedClassName.endswith("BreadcrumbBarItem"):
 				obj.roleText = obj.UIAElement.currentLocalizedControlType
 			# Taskbar/search combo box in 22H2 Moment 2 (February 2023) has no label.
-			# Resolved in build 25309.
+			# Resolved in build 25309, eventually backported to build 22621.1483.
+			# Resolution involves search grouping (obj.parent) with a specific Automation Id.
 			elif (
 				automationId == "SystemSettings_DesktopTaskbar_Search_ComboBox"
 				and not obj.name
-				and winVersion.getWinVer().build < 25309
+				and obj.parent.UIAAutomationId != "SystemSettings_DesktopTaskbar_Search_EntityItem"
 			):
 				obj.name = obj.previous.name
 
