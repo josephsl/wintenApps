c8467233fbcd1c56c5b22abbc1c7a1ab6be722a8
Joseph Lee <joseph.lee22590@gmail.com>
2023-04-07 13:52:40 -0700

    Explorer/Windows 11 taskbar: use wx.CallLater rather than wx.CallAfter when announcing rearranged taskbar item positions.
    NVDA 2023.2 changes input processing internals, causing NVDA to say odd things when rearranging taskbar items in Windows 11. An alternative approach is calling taskbar position announcer about a millisecond after Alt+Shift+left/right arrows are pressed to rearrange taskbar items, which also works in NVDA 2023.1 and earlier, hence use CallLater method.

--- addon/appModules/explorer.py
+++ addon/appModules/explorer.py
@@ -62,14 +62,14 @@ class TaskbarItem(NVDAObject):
 		announcePosition = winVersion.getWinVer() >= winVersion.WIN11 and isinstance(self.next, TaskbarItem)
 		gesture.send()
 		if announcePosition:
-			wx.CallAfter(self.announceDragPosition)
+			wx.CallLater(1, self.announceDragPosition)
 
 	@scriptHandler.script(gesture="kb:alt+shift+leftArrow")
 	def script_moveLeft(self, gesture) -> None:
 		announcePosition = winVersion.getWinVer() >= winVersion.WIN11 and isinstance(self.previous, TaskbarItem)
 		gesture.send()
 		if announcePosition:
-			wx.CallAfter(self.announceDragPosition)
+			wx.CallLater(1, self.announceDragPosition)
 
 
 # App module class comes from built-in File Explorer app module but Mypy doesn't know that.
