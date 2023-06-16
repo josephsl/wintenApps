1ff1e176ddf6ca55eb42d84ebe929e197d28be38
Joseph Lee <joseph.lee22590@gmail.com>
2023-06-15 19:47:32 -0700

    Shell Experience Host: add type hints for NVDA object init event to align with other dd-on modules

--- addon/appModules/shellexperiencehost.py
+++ addon/appModules/shellexperiencehost.py
@@ -11,12 +11,13 @@ Shell Experience Host is home to a number of things, including Action Center and
 from nvdaBuiltin.appModules.shellexperiencehost import AppModule
 from NVDAObjects.UIA import UIA
 import controlTypes
+from NVDAObjects import NVDAObject
 
 
 # App module class comes from built-in Shell Experience Host app module but Mypy doesn't know that.
 class AppModule(AppModule):  # type: ignore[no-redef]
 
-	def event_NVDAObject_init(self, obj):
+	def event_NVDAObject_init(self, obj: NVDAObject):
 		if isinstance(obj, UIA):
 			# Volume mixer list items in canary build 25300 series have no label.
 			# There is no Automation Id but a generic class name is used, so locate items via its children.
