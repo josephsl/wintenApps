57710f142b655aef47c6cc1c3c4f0c56e89fee45
Joseph Lee <joseph.lee22590@gmail.com>
2023-06-24 15:37:07 -0700

    WinApps/taskbar items (Flake8 F821): bring in needed imports from File Explorer ap module.
    Bring in needed imports for taskbar item class form File Explorer app module.

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -3,14 +3,17 @@
 
 # Adds handlers for various UIA controls found in Windows 10 and later.
 
-from typing import List, Optional
+from typing import List, Optional, Dict
 import globalPluginHandler
-from NVDAObjects.UIA import Dialog
+from NVDAObjects.UIA import UIA, Dialog
 from NVDAObjects import NVDAObject
 import NVDAObjects
 import globalVars
 import UIAHandler
 import eventHandler
+import ui
+import scriptHandler
+import core
 
 
 # Virtual desktop announcements come from a combination of this add-on and an NVDA Core pull request.
