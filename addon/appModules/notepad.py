6473605ffa0639d56485ae898b7776771f9c6cab
Joseph Lee <joseph.lee22590@gmail.com>
2023-07-23 22:35:31 -0700

    Notepad: remove status bar announcement workaround.
    NVDA 2023.2 includes status bar announcement fix for newer (tabbed) Notepad 11, therefore remove it from the add-on.

--- addon/appModules/notepad.py
+++ addon/appModules/notepad.py
@@ -43,37 +43,3 @@ class AppModule(appModuleHandler.AppModule):
 				)
 			)
 		nextHandler()
-
-	def _get_statusBar(self) -> NVDAObject:
-		"""Retrieves Windows 11 Notepad status bar.
-		In Windows 10 and earlier, status bar can be obtained by looking at the bottom of the screen.
-		Windows 11 Notepad uses Windows 11 UI design (top-level window is labeled "DesktopWindowXamlSource",
-		therefore status bar cannot be obtained by position alone.
-		If visible, a child of the foreground window hosts the status bar elements.
-		Status bar child position must be checked whenever Notepad is updated on stable Windows 11 releases
-		as Notepad is updated through Microsoft Store as opposed to tied to specific Windows releases.
-		L{api.getStatusBar} will resort to position lookup if C{NotImplementedError} is raised.
-		"""
-		# #13688: Notepad 11 uses Windows 11 user interface, therefore status bar is harder to obtain.
-		# This does not affect earlier versions.
-		notepadVersion = int(self.productVersion.split(".")[0])
-		if notepadVersion < 11:
-			raise NotImplementedError()
-		# And no, status bar is shown when editing documents.
-		# Thankfully, of all the UIA objects encountered, document window has a unique window class name.
-		if api.getFocusObject().windowClassName != "RichEditD2DPT":
-			raise NotImplementedError()
-		# Obtain status bar text across Notepad 11 releases.
-		# Resolved in NVDA 2023.2 (remove this method completely).
-		clientObject = UIAHandler.handler.clientObject
-		condition = clientObject.createPropertyCondition(UIAHandler.UIA_AutomationIdPropertyId, "ContentTextBlock")
-		walker = clientObject.createTreeWalker(condition)
-		notepadWindow = clientObject.elementFromHandle(api.getForegroundObject().windowHandle)
-		try:
-			element = walker.getFirstChildElement(notepadWindow)
-			# Is status bar showing?
-			element = element.buildUpdatedCache(UIAHandler.handler.baseCacheRequest)
-		except (ValueError, COMError):
-			raise NotImplementedError
-		statusBar = UIA(UIAElement=element).parent
-		return statusBar
