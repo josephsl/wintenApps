3af8135cd35858a9f9b9767052bb95e0220ee474
Joseph Lee <joseph.lee22590@gmail.com>
2023-09-16 20:13:27 -0600

    BuildVars: update add-on URL to point to GitHub.
    Discussed on NvDA dd-ons list: addons.nvda-project.org website is now considered old and legacy as NV Access add-on store is operational. Therefore switch add-on URL (homepage) to GitHub instead of community add-ons website.

--- buildVars.py
+++ buildVars.py
@@ -29,7 +29,7 @@ addon_info = {
 	# Author(s)
 	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>, Derek Riemer <driemer.riemer@gmail.com> and others",
 	# URL for the add-on documentation support
-	"addon_url": "https://addons.nvda-project.org/",
+	"addon_url": "https://github.com/josephsl/wintenApps",
 	# URL for the add-on repository where the source code can be found
 	"addon_sourceURL": "https://github.com/josephsl/wintenApps",
 	# Documentation file name
