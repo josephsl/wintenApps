701b56e36feb93c6cbbb63b7e9a478ef654ec3cd
Joseph Lee <joseph.lee22590@gmail.com>
2023-11-23 10:52:32 -0700

    Settings: workarounds for -> applies to Windows 10

--- addon/appModules/systemsettings.py
+++ addon/appModules/systemsettings.py
@@ -19,7 +19,7 @@ from NVDAObjects import NVDAObject
 class AppModule(AppModule):  # type: ignore[no-redef]
 
 	def event_liveRegionChange(self, obj: NVDAObject, nextHandler: Callable[[], None]):
-		# Workarounds for Windows 10
+		# Applies to Windows 10
 		if winVersion.getWinVer() < winVersion.WIN11:
 			automationId = obj.UIAAutomationId
 			# Except for specific cases, announce all live regions.
