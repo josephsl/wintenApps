570edeb053b346b467ad8295a9a8941a4b8ec5ca
Joseph Lee <joseph.lee22590@gmail.com>
2023-12-21 13:40:39 -0700

    Update copyright header

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,5 +1,5 @@
 # WinApps/installTasks.py
-# Copyright 2016-2023 Joseph Lee, released under GPL.
+# Copyright 2016-2024 Joseph Lee, released under GPL.
 
 # Provides needed routines during add-on installation and removal.
 # Mostly checks compatibility.
