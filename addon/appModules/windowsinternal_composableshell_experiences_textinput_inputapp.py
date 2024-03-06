0445a7f6400c0e211707cee614c72c59dade3de4
Joseph Lee <joseph.lee22590@gmail.com>
2024-03-06 11:09:37 -0700

    Modern keyboard/window open event: check to make sure IME candidate item is not None.
    Actual problem with attribute error: due to traversal issues, IME candidate item is None, causing attribute error to be raised. Therefore, before removing/adding UIA elements to local event handler group, check to make sure IME candidate item is indeed something other than None.

--- addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -60,17 +60,15 @@ class AppModule(AppModule):  # type: ignore[no-redef]
 			if winVersion.getWinVer() >= winVersion.WIN11:
 				# For some odd reason, suggested text is the last element.
 				imeCandidateItem = imeCandidateItem.lastChild
-			try:
-				# Sometimes traversal fails, resulting in a null element being added.
-				# Noticeable when opening Voice Access suggestions in Windows 11.
+			# Sometimes traversal fails, resulting in a null element being added.
+			# Noticeable when opening Voice Access suggestions in Windows 11.
+			if imeCandidateItem is not None:
 				UIAHandler.handler.removeEventHandlerGroup(
 					imeCandidateItem.UIAElement, UIAHandler.handler.localEventHandlerGroup
 				)
 				UIAHandler.handler.addEventHandlerGroup(
 					imeCandidateItem.UIAElement, UIAHandler.handler.localEventHandlerGroup
 				)
-			except AttributeError:
-				pass
 		# NVDA Core takes care of the rest.
 		super().event_UIA_window_windowOpen(obj, nextHandler)
 
