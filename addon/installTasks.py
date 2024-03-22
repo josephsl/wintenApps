02f6a3b3f385470adddeb7382fbfe14c09b60886
Joseph Lee <joseph.lee22590@gmail.com>
2024-03-21 20:27:42 -0600

    Install tasks/error message: add a dedicated error message for 32-bit Windows 10 detection.
    Present a message when trying to install the add-on on 32-bit Windows 10 systems. The wording wil reference Windows 10 (it is highly unlikely that Windows 8.1 32-bit users wil attempt to install the add-on). A flag wil be passed ito message presentation function to show either the generic 'unsupporte Windows release' message or the 32-bit specific message.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -32,29 +32,37 @@ def canInstallWinAppsAddon(currentWinVer: winVersion.WinVersion) -> bool:
 	)
 
 
-def presentInstallError(currentWinVer: winVersion.WinVersion) -> None:
+def presentInstallError(currentWinVer: winVersion.WinVersion, is32bit: bool = False) -> None:
 	import gui
 	import wx
 	import gettext
 	_ = gettext.gettext
 	# Translators: title of the error dialog shown when trying to install the add-on in unsupported systems.
-	# Unsupported systems include Windows versions earlier than 10 and unsupported feature updates.
+	# Unsupported systems include Windows versions earlier than 10, 32-bit Windows 10,
+	# unsupported feature updates.
 	unsupportedWindowsReleaseTitle: str = _("Unsupported Windows release")
-	# #78: obtain a list of all supported releases (and builds) from supported releases list.
-	# Present releases above the current release if possible.
-	# For example, present Windows 11 releases if this is a release above Windows 10 22H2 (19045).
-	minimumWinVer: winVersion.WinVersion = min(entry for entry in SUPPORTED_RELEASES if entry > currentWinVer)
-	unsupportedWindowsReleaseText: str = _(
-		# Translators: Dialog text shown when trying to install the add-on on an unsupported Windows release.
-		# Release name and build refer to Windows release in use (example: Windows 10 21H2 (19044)).
-		"You are using {releaseName} ({build}), a Windows release not supported by this add-on.\n"
-		"This add-on requires {supportedReleaseName} ({supportedBuild}) or later."
-	).format(
-		releaseName=currentWinVer.releaseName,
-		build=currentWinVer.build,
-		supportedReleaseName=minimumWinVer.releaseName,
-		supportedBuild=minimumWinVer.build
-	)
+	# Detect 32-bit Windows 10 until Windows 10 series is no longer supported by the add-on.
+	if is32bit:
+		unsupportedWindowsReleaseText: str = _(
+			# Translators: Dialog text shown when trying to install the add-on on 32-bit Windows 10.
+			"You are using 32-bit Windows 10. This add-on requires 64-bit Windows 10 or later."
+		)
+	else:
+		# #78: obtain a list of all supported releases (and builds) from supported releases list.
+		# Present releases above the current release if possible.
+		# For example, present Windows 11 releases if this is a release above Windows 10 22H2 (19045).
+		minimumWinVer: winVersion.WinVersion = min(entry for entry in SUPPORTED_RELEASES if entry > currentWinVer)
+		unsupportedWindowsReleaseText: str = _(
+			# Translators: Dialog text shown when trying to install the add-on on an unsupported Windows release.
+			# Release name and build refer to Windows release in use (example: Windows 10 21H2 (19044)).
+			"You are using {releaseName} ({build}), a Windows release not supported by this add-on.\n"
+			"This add-on requires {supportedReleaseName} ({supportedBuild}) or later."
+		).format(
+			releaseName=currentWinVer.releaseName,
+			build=currentWinVer.build,
+			supportedReleaseName=minimumWinVer.releaseName,
+			supportedBuild=minimumWinVer.build
+		)
 	gui.messageBox(unsupportedWindowsReleaseText, unsupportedWindowsReleaseTitle, wx.OK | wx.ICON_ERROR)
 
 
