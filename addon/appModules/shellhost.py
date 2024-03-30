810a25d42d2eef925a735a081bedc7f4a31e8889
Joseph Lee <joseph.lee22590@gmail.com>
2024-03-30 12:20:46 -0600

    Shell host: mark control center window UIA window workaround as NVDA Core issue 16348

--- addon/appModules/shellhost.py
+++ addon/appModules/shellhost.py
@@ -11,6 +11,7 @@ from winAPI.types import HWNDValT
 class AppModule(appModuleHandler.AppModule):
 
 	def isGoodUIAWindow(self, hwnd: HWNDValT) -> bool:
+		# NVDA Core issue 16348: reclassify control center window as UIA to allow mouse/touch interaction.
 		if winUser.getClassName(hwnd) == "ControlCenterWindow":
 			return True
 		return False
