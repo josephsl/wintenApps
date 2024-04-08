e9ed842841a829c891caf257b7fc677137b9a979
Joseph Lee <joseph.lee22590@gmail.com>
2024-04-07 20:20:40 -0600

    ShellExperienceHost: mark Control Center Window workarond as NVDA 2024.2 bug fix

--- addon/appModules/shellexperiencehost.py
+++ addon/appModules/shellexperiencehost.py
@@ -16,6 +16,7 @@ class AppModule(AppModule):  # type: ignore[no-redef]
 
 	def isGoodUIAWindow(self, hwnd: HWNDValT) -> bool:
 		# NVDA Core issue 16348: reclassify control center window as UIA to allow mouse/touch interaction.
+		# Resolved in NVDA 2024.2.
 		if winUser.getClassName(hwnd) == "ControlCenterWindow":
 			return True
 		return False
