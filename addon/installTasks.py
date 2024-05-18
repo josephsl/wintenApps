c2cb138da55fafed23cce663be56065f9c2f9de4
Joseph Lee <joseph.lee22590@gmail.com>
2024-05-18 15:00:51 -0600

    Install tasks: integrate error presentation function back into onInstall function. Re #93.
    Bring back error presentation function inside onInstall function as there is no longer a need to leave room for additional dialogs or checks beyond Windows release error message (for now).

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -58,7 +58,25 @@ def presentInstallError(currentWinVer: winVersion.WinVersion) -> None:
 def onInstall() -> None:
 	currentWinVer = winVersion.getWinVer()
 	if not canInstallWinAppsAddon(currentWinVer):
-		presentInstallError(currentWinVer)
+		import gui
+		import wx
+		# Translators: title of the error dialog shown when trying to install the add-on in
+		# unsupported Windows systems (earlier than 10, 32-bit Windows 10, unsupported feature updates).
+		unsupportedWindowsReleaseTitle: str = _("Unsupported Windows release")
+		# #78: present the supported version above the current release.
+		minimumWinVer: winVersion.WinVersion = min(entry for entry in SUPPORTED_RELEASES if entry > currentWinVer)
+		unsupportedWindowsReleaseText: str = _(
+			# Translators: Dialog text shown when trying to install the add-on on
+			# releases earlier than minimum supported release.
+			"You are using {releaseName} ({build}), a Windows release not supported by this add-on.\n"
+			"This add-on requires {supportedReleaseName} ({supportedBuild}) or later."
+		).format(
+			releaseName=currentWinVer.releaseName,
+			build=currentWinVer.build,
+			supportedReleaseName=minimumWinVer.releaseName,
+			supportedBuild=minimumWinVer.build
+		)
+		gui.messageBox(unsupportedWindowsReleaseText, unsupportedWindowsReleaseTitle, wx.OK | wx.ICON_ERROR)
 		raise RuntimeError(
 			f"Windows App Essentials does not support {currentWinVer.releaseName} ({currentWinVer.build})"
 		)
