be9e068973cec84dd723baf8ffe702e6bb01e6b8
Joseph Lee <joseph.lee22590@gmail.com>
2024-05-18 15:08:27 -0600

    Install tasks: remove integrated functions from the module. Fixes #93

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -25,38 +25,6 @@ SUPPORTED_RELEASES: list[winVersion.WinVersion] = [
 ]
 
 
-def canInstallWinAppsAddon(currentWinVer: winVersion.WinVersion) -> bool:
-	return (
-		currentWinVer in SUPPORTED_RELEASES  # General availability channel and Insider release preview
-		or currentWinVer > max(SUPPORTED_RELEASES)  # Insider Preview canary/dev/beta
-	)
-
-
-def presentInstallError(currentWinVer: winVersion.WinVersion) -> None:
-	import gui
-	import wx
-	# Translators: title of the error dialog shown when trying to install the add-on in unsupported systems.
-	# Unsupported systems include Windows versions earlier than 10, 32-bit Windows 10,
-	# and unsupported feature updates.
-	unsupportedWindowsReleaseTitle: str = _("Unsupported Windows release")
-	# #78: obtain a list of all supported releases (and builds) from supported releases list.
-	# Present releases above the current release if possible.
-	# For example, present Windows 11 releases if this is a release above Windows 10 22H2 (19045).
-	minimumWinVer: winVersion.WinVersion = min(entry for entry in SUPPORTED_RELEASES if entry > currentWinVer)
-	unsupportedWindowsReleaseText: str = _(
-		# Translators: Dialog text shown when trying to install the add-on on an unsupported Windows release.
-		# Release name and build refer to Windows release in use (example: Windows 10 21H2 (19044)).
-		"You are using {releaseName} ({build}), a Windows release not supported by this add-on.\n"
-		"This add-on requires {supportedReleaseName} ({supportedBuild}) or later."
-	).format(
-		releaseName=currentWinVer.releaseName,
-		build=currentWinVer.build,
-		supportedReleaseName=minimumWinVer.releaseName,
-		supportedBuild=minimumWinVer.build
-	)
-	gui.messageBox(unsupportedWindowsReleaseText, unsupportedWindowsReleaseTitle, wx.OK | wx.ICON_ERROR)
-
-
 def onInstall() -> None:
 	currentWinVer = winVersion.getWinVer()
 	if not (
