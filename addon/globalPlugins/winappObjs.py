aa0d50e26aa241966c3de6e48597ed0451ffe84d
Joseph Lee <joseph.lee22590@gmail.com>
2024-05-18 12:27:53 -0600

    WinApps: add a note on its exclusion from add-on installation package.
    While no loner included in the add-on package (for now), WinApps global plugin is kept in the source code repository to remind people about the initial purpose and scope of the add-on (covering controls across applications in Windows 10 and later).

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -2,6 +2,7 @@
 # Copyright 2015-2024 Joseph Lee, released under GPL.
 
 # Adds handlers for various UIA controls found in Windows 10 and later.
+# No longer included in the add-on package but kept in code repository.
 
 import globalPluginHandler
 
