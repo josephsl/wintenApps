bb5fd868fbde8428e03c2e602fc12084a3bf91d9
Joseph Lee <joseph.lee22590@gmail.com>
2024-07-17 01:26:44 -0600

    Voice access: app odule returns (based on add-on 23.11 with updated headers and removal of annotations import)

---
+++ addon/appModules/voiceaccess.py
@@ -0,0 +1,44 @@
+# Voice access
+# Part of Windows App Essentials collection
+# Copyright 2022-2024 Joseph Lee, released under GPL
+
+# Support for Voice access on Windows 11.
+
+import appModuleHandler
+import scriptHandler
+import ui
+import winUser
+from NVDAObjects.UIA import UIA, Dialog
+from NVDAObjects import NVDAObject
+from winAPI.types import HWNDValT
+
+
+# Deprecated: inaccurate announcements (perhaps due to input timing)
+# and cannot be toggled via object navigation/touch (can be toggled using keyboard and mouse, however).
+class MicrophoneButton(UIA):
+
+	def announceMicStatus(self):
+		ui.message(self.name)
+
+	@scriptHandler.script(gestures=["kb:space", "kb:enter", "kb:numpadEnter"])
+	def script_toggleMicrophone(self, gesture) -> None:
+		# Until NVDA gets a chance to handle UIA notifications when these commands are pressed...
+		gesture.send()
+		import core
+		core.callLater(1, self.announceMicStatus)
+
+
+class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj: NVDAObject, clsList: list[NVDAObject]) -> None:
+		if isinstance(obj, UIA):
+			if obj.UIAElement.cachedClassName == "Popup":
+				clsList.insert(0, Dialog)
+			elif obj.UIAAutomationId == "MicrophoneButtonControl":
+				clsList.insert(0, MicrophoneButton)
+
+	def isGoodUIAWindow(self, hwnd: HWNDValT) -> bool:
+		# #72: allow proper mouse and touch interaction from main Voice access interface.
+		if winUser.getClassName(hwnd) == "Voice access":
+			return True
+		return False
