546abdfa95bc45cb25bae40cb5fc5d5ecdb14ef6
Joseph Lee <joseph.lee22590@gmail.com>
2024-09-27 22:30:36 -0600

    Install tasks: remove additional and now redundant conditional statement dealing with Windows 11 releases.
    As minimum Windows version is set using a ternary operation, there is no need to handle Windows 11 separately.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -23,9 +23,6 @@ def onInstall() -> None:
 	# Minimum: 64-bit Windows 10 22H2 (final Windows 10 feature update, supported until October 2025).
 	# Recommended: Windows 11 23H2 or later.
 	minimumWinVer = winVersion.WIN11_23H2 if (currentWinVer := winVersion.getWinVer()) > winVersion.WIN10_22H2 else winVersion.WIN10_22H2
-	if (currentWinVer := winVersion.getWinVer()) > minimumWinVer:
-		# Recommended: Windows 11 23H2 or later.
-		minimumWinVer = winVersion.WIN11_23H2
 	if currentWinVer < minimumWinVer:
 		gui.messageBox(
 			_(
