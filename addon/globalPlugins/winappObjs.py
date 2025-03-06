b7372cae54a653a18bd2fb6d52eb847be39cdf37
Joseph Lee <joseph.lee22590@gmail.com>
2025-03-06 12:36:31 -0700

    WinApps: restore secure mode check.
    Because UIA handler wil be patched, do not allow the add-on to run in secure mode.

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -4,9 +4,16 @@
 # Adds handlers for various UIA controls found in Windows 10 and later.
 
 import globalPluginHandler
+import globalVars
 import UIAHandler
 
 
+# Don't even think about proceeding in secure screens.
+def disableInSecureMode(cls):
+	return globalPluginHandler.GlobalPlugin if globalVars.appArgs.secure else cls
+
+
+@disableInSecureMode
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def __init__(self):
 		super().__init__()
