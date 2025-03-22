74287d39e8ceb7648550f3bc7422da4144ec06fe
Joseph Lee <joseph.lee22590@gmail.com>
2025-03-21 23:54:31 -0600

    WinApps: Windows 10 or later -> Windows 11

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -1,7 +1,7 @@
 # Windows app controls repository
 # Copyright 2015-2025 Joseph Lee, released under GPL.
 
-# Adds handlers for various UIA controls found in Windows 10 and later.
+# Adds handlers for various UIA controls found in Windows 11.
 
 import globalPluginHandler
 import globalVars
