b6f8d3c410aee6e0e35915f51b24a17081ce5e6a
Joseph Lee <joseph.lee22590@gmail.com>
2025-04-06 20:08:45 -0600

    WinApps: remove good UIA window class names hack from global plugin constructor

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -5,8 +5,6 @@
 
 import globalPluginHandler
 import globalVars
-import UIAHandler
-from logHandler import log
 
 
 # Don't even think about proceeding in secure screens.
@@ -18,13 +16,3 @@ def disableInSecureMode(cls):
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def __init__(self):
 		super().__init__()
-		# NVDA Core issues 17407 and 17771 (hack):
-		# add WinUI3 top-level class name to good UIA window classes tuple to enable mouse/touch navigation.
-		# Resolved in NVDA 2025.2.
-		if "Microsoft.UI.Content.DesktopChildSiteBridge" not in UIAHandler.goodUIAWindowClassNames:
-			log.debug(
-				"winapps: adding Microsoft.UI.Content.DesktopChildSiteBridge to good UIA window class names"
-			)
-			goodUIAWindowClassNames = set(UIAHandler.goodUIAWindowClassNames)
-			goodUIAWindowClassNames.add("Microsoft.UI.Content.DesktopChildSiteBridge")
-			UIAHandler.goodUIAWindowClassNames = tuple(goodUIAWindowClassNames)
