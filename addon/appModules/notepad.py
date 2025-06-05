66110cc1b43a5f247cbd21e912695df7856d6eb6
Joseph Lee <joseph.lee22590@gmail.com>
2025-06-05 12:18:04 -0600

    Notepad: remove NVDA Core code.
    The bulk of the Notepad app module is part of NVDA Core, therefore remove these. As a result, built-in Notepad app module will be imported and as a different module to avoid type issues.

--- addon/appModules/notepad.py
+++ addon/appModules/notepad.py
@@ -7,76 +7,14 @@
 While this app module also covers older Notepad releases,
 this module provides workarounds for Windows 11 Notepad."""
 
-from typing import Callable
-from comtypes import COMError
-import appModuleHandler
-import api
-import braille
+# Import the base Notepad app module as something else to avoid runtime issues.
+from nvdaBuiltin.appModules.notepad import AppModule as CoreAppModule
 import controlTypes
-import eventHandler
-import speech
-import UIAHandler
 from NVDAObjects.UIA import UIA
 from NVDAObjects import NVDAObject
 
 
-class AppModule(appModuleHandler.AppModule):
-	def event_UIA_elementSelected(self, obj: NVDAObject, nextHandler: Callable[[], None]):
-		# NVDA Core issue 14587/PR 14588: announce tab switches in Notepad 11.2212 and later.
-		# Element selected event fires multiple times due to state changes.
-		# Resolved in NVDA 2023.2 (remove this method completely).
-		if (
-			obj.role == controlTypes.Role.TAB
-			# this is done because 2 selection events are sent for the same object, so to prevent double speaking.
-			and not eventHandler.isPendingEvents("UIA_elementSelected")
-			and controlTypes.State.SELECTED in obj.states
-		):
-			speech.cancelSpeech()
-			speech.speakObject(obj, reason=controlTypes.OutputReason.FOCUS)
-			braille.handler.message(
-				braille.getPropertiesBraille(
-					name=obj.name,
-					role=obj.role,
-					states=obj.states,
-					positionInfo=obj.positionInfo
-				)
-			)
-		nextHandler()
-
-	def _get_statusBar(self) -> NVDAObject:
-		"""Retrieves Windows 11 Notepad status bar.
-		In Windows 10 and earlier, status bar can be obtained by looking at the bottom of the screen.
-		Windows 11 Notepad uses Windows 11 UI design (top-level window is labeled "DesktopWindowXamlSource",
-		therefore status bar cannot be obtained by position alone.
-		If visible, a child of the foreground window hosts the status bar elements.
-		Status bar child position must be checked whenever Notepad is updated on stable Windows 11 releases
-		as Notepad is updated through Microsoft Store as opposed to tied to specific Windows releases.
-		L{api.getStatusBar} will resort to position lookup if C{NotImplementedError} is raised.
-		"""
-		# #13688: Notepad 11 uses Windows 11 user interface, therefore status bar is harder to obtain.
-		# This does not affect earlier versions.
-		notepadVersion = int(self.productVersion.split(".")[0])
-		if notepadVersion < 11:
-			raise NotImplementedError()
-		# And no, status bar is shown when editing documents.
-		# Thankfully, of all the UIA objects encountered, document window has a unique window class name.
-		if api.getFocusObject().windowClassName != "RichEditD2DPT":
-			raise NotImplementedError()
-		# Obtain status bar text across Notepad 11 releases.
-		# Resolved in NVDA 2023.2 (remove this method completely).
-		clientObject = UIAHandler.handler.clientObject
-		condition = clientObject.createPropertyCondition(UIAHandler.UIA_AutomationIdPropertyId, "ContentTextBlock")
-		walker = clientObject.createTreeWalker(condition)
-		notepadWindow = clientObject.elementFromHandle(api.getForegroundObject().windowHandle)
-		try:
-			element = walker.getFirstChildElement(notepadWindow)
-			# Is status bar showing?
-			element = element.buildUpdatedCache(UIAHandler.handler.baseCacheRequest)
-		except (ValueError, COMError):
-			raise NotImplementedError
-		statusBar = UIA(UIAElement=element).parent
-		return statusBar
-
+class AppModule(CoreAppModule):
 	def event_NVDAObject_init(self, obj: NVDAObject):
 		# NVDA Core issue 18208: "go to line" edit field is classified as a dialog.
 		if isinstance(obj, UIA) and obj.UIAAutomationId == "LineNumberBox":
