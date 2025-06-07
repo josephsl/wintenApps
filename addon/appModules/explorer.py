96402e73f0db477c88f4bd7150948de10a0d0c03
Joseph Lee <joseph.lee22590@gmail.com>
2025-06-07 05:09:35 -0600

    File Explorer: remove UIA element selected event (part of NVDA 2023.2)

--- addon/appModules/explorer.py
+++ addon/appModules/explorer.py
@@ -9,7 +9,6 @@
 from typing import Callable
 # Flake8 F403: detect other add-ons that overrode File Explorer app module.
 from nvdaBuiltin.appModules.explorer import *  # NOQA: F403
-import controlTypes
 import ui
 from NVDAObjects import NVDAObject
 
@@ -17,31 +16,6 @@ from NVDAObjects import NVDAObject
 # App module class comes from built-in File Explorer app module but Mypy doesn't know that.
 # Also tell Flake8 that the base AppModule class comes from NVDA Core.
 class AppModule(AppModule):  # type: ignore[misc]  # NOQA: F405
-	def event_UIA_elementSelected(self, obj: NVDAObject, nextHandler: Callable[[], None]):
-		# NVDA Core issue 14388: announce File Explorer tab switches (Windows 11 22H2 and later).
-		# Resolved in NVDA 2023.2 (remove this method completely).
-		import speech
-		import braille
-		import eventHandler
-		# Element selected event fires multiple times due to state changes.
-		if (
-			obj.role == controlTypes.Role.TAB
-			and controlTypes.State.SELECTED in obj.states
-			and obj.parent.UIAAutomationId == "TabListView"
-			# this is done because 2 selection events are sent for the same object, so to prevent double speaking.
-			and not eventHandler.isPendingEvents(eventName="UIA_elementSelected")
-		):
-			speech.speakObject(obj, reason=controlTypes.OutputReason.FOCUS)
-			braille.handler.message(
-				braille.getPropertiesBraille(
-					name=obj.name,
-					role=obj.role,
-					states=obj.states,
-					positionInfo=obj.positionInfo
-				)
-			)
-		nextHandler()
-
 	def shouldProcessUIANotificationEventNoWindowHandle(
 		self,
 		sender,
