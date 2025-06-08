047eeb3e68bd990ef108ad573e65dcd7791d9685
Joseph Lee <joseph.lee22590@gmail.com>
2025-06-08 09:44:13 -0600

    WinApps: add a base implementation of 'shouldProcessUIANotificationEvent'.
    Add a base (app module like) implementation of 'shouldProcessUIANotificationEvent' that returns a type converted bool if a nearest window handle was found (True if found, False otherwise). If there is no window handle, the nearest handle method inside UIA handler returns None, which is conveniently converted to False.

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -112,6 +112,26 @@ class UIAHandlerEx(UIAHandler.UIAHandler):
 		)
 
 
+def winapps_shouldProcessUIANotificationEvent(
+	sender,
+	NotificationKind: int | None = None,
+	NotificationProcessing: int | None = None,
+	displayString: str = "",
+	activityId: str = "",
+) -> bool:
+	"""
+	Determines whether NVDA should process a UIA notification event
+	By default, events from elements with window handle value set
+	and traversable back to the desktop will be accepted.
+	Returning False will cause the event to be dropped completely.
+	Returning True means that the event will be processed, but it might still
+	be rejected later; e.g. because it isn't native UIA, because
+	shouldAcceptEvent returns False, etc.
+	"""
+	# By default, see if UIA tree can be traversed to arrive at the desktop element.
+	return bool(UIAHandler.handler.getNearestWindowHandle(sender))
+
+
 # Don't even think about proceeding in secure screens.
 def disableInSecureMode(cls):
 	return globalPluginHandler.GlobalPlugin if globalVars.appArgs.secure else cls
