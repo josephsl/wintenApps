6ffd9e1cbaa5a7d1f31e2a77d298e36faab02f64
Joseph Lee <joseph.lee22590@gmail.com>
2025-06-10 14:13:12 -0600

    Modern eyboard: remove NVDA object init event handler (again; included in NVDA 2025.1)

--- addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -10,27 +10,13 @@
 # (formerly WindowsInternal.ComposableShell.Experiences.TextInput.InputApp).
 # #70: NVDA Core pull requests are made using the core app module, not alias modules.
 from nvdaBuiltin.appModules.windowsinternal_composableshell_experiences_textinput_inputapp import AppModule
-import controlTypes
 import api
 from NVDAObjects import NVDAObject
 
 
 # Built-in modern keyboard app module powers bulk of the below app module class, so inform Mypy.
 class AppModule(AppModule):  # type: ignore[no-redef]
-
 	def event_UIA_elementSelected(self, obj, nextHandler):
 		if api.getFocusObject().appModule == self:
 			return nextHandler()
 		super().event_UIA_elementSelected(obj, nextHandler)
-
-	def event_NVDAObject_init(self, obj: NVDAObject) -> None:
-		# NVDA Core issue 17308: recent Windows 11 builds raise live region change event when
-		# clipboard history closes, causing NVDA to report data item text such as clipboard history entries.
-		# Therefore, tell NVDA to veto this event at the object level, otherwise focus change handling breaks
-		# due to live region change event being queued.
-		# Resolved in NVDA 2025.1.
-		if obj.role == controlTypes.Role.DATAITEM and obj.parent.role in (
-			controlTypes.Role.TABLEROW,  # Clipboard history item
-			controlTypes.Role.LIST,  # Clipboard history item actions list
-		):
-			obj._shouldAllowUIALiveRegionChangeEvent = False
