82c2fb813c1b102526cc3de61982214eeaa3bebd
Joseph Lee <joseph.lee22590@gmail.com>
2025-06-10 14:16:23 -0600

    Modern keyboard/UIA element selected event: add type hints for event handler arguments

--- addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -10,13 +10,14 @@
 # (formerly WindowsInternal.ComposableShell.Experiences.TextInput.InputApp).
 # #70: NVDA Core pull requests are made using the core app module, not alias modules.
 from nvdaBuiltin.appModules.windowsinternal_composableshell_experiences_textinput_inputapp import AppModule
+from typing import Callable
 import api
 from NVDAObjects import NVDAObject
 
 
 # Built-in modern keyboard app module powers bulk of the below app module class, so inform Mypy.
 class AppModule(AppModule):  # type: ignore[no-redef]
-	def event_UIA_elementSelected(self, obj, nextHandler):
+	def event_UIA_elementSelected(self, obj: NVDAObject, nextHandler: Callable[[], None]):
 		if api.getFocusObject().appModule == self:
 			return nextHandler()
 		super().event_UIA_elementSelected(obj, nextHandler)
