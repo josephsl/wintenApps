86070637e99f8bbc7a20f2bf5fa62eb383876339
Joseph Lee <joseph.lee22590@gmail.com>
2025-06-10 14:00:03 -0600

    Modern keyboard: resolve speech repetitions in Widows 11 emoji panel.
    The base NVDA object includes selection event handler to respond to events such as UIA element selected. The base object implementation will announce selected item via spech and braile if it is focused somehow. However, if an app module includes its own speech and raille output routine for selected item, this can result in NVDA announcing the selected item twice. This is the case in Windows 11 emoji panel where the panel takes system focus when laucnhed, and selected panel item will be announced up to two times. Therefore, let the base NVDA (UIA) object handle selection event when the emoji panel raises UIA element selected event, thereby resolving the repeated speech issue. Also, as a side effect, this improves element selected event performance as NVDA does not have to traverse the entire event handler just to announce the selected item when the base NVDA object will report the focused/selected item.

--- addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
+++ addon/appModules/windowsinternal_composableshell_experiences_textinput_inputapp.py
@@ -11,11 +11,18 @@
 # #70: NVDA Core pull requests are made using the core app module, not alias modules.
 from nvdaBuiltin.appModules.windowsinternal_composableshell_experiences_textinput_inputapp import AppModule
 import controlTypes
+import api
 from NVDAObjects import NVDAObject
 
 
 # Built-in modern keyboard app module powers bulk of the below app module class, so inform Mypy.
 class AppModule(AppModule):  # type: ignore[no-redef]
+
+	def event_UIA_elementSelected(self, obj, nextHandler):
+		if api.getFocusObject().appModule == self:
+			return nextHandler()
+		super().event_UIA_elementSelected(obj, nextHandler)
+
 	def event_NVDAObject_init(self, obj: NVDAObject) -> None:
 		# NVDA Core issue 17308: recent Windows 11 builds raise live region change event when
 		# clipboard history closes, causing NVDA to report data item text such as clipboard history entries.
