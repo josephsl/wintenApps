b556e85c573e098ae2c2bcf724e33ad0b223824d
Joseph Lee <joseph.lee22590@gmail.com>
2025-06-28 11:56:57 -0600

    WinApps: remove global plugin version of should process UIA notification event (meant for app modules).
    NVDA 2025.2 will let app modules say yes or no when asked to process UIA notification events for elements without window handles. Therefore, remove the global plugin version.

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -14,28 +14,6 @@ import winVersion
 from logHandler import log
 
 
-# Determine UIA notification processing on behalf of app modules.
-# Resolved in NVDA 2025.2 (part of app modules).
-def winapps_shouldProcessUIANotificationEvent(
-	sender: UIAHandler.UIA.IUIAutomationElement,
-	notificationKind: int | None = None,
-	notificationProcessing: int | None = None,
-	displayString: str = "",
-	activityId: str = "",
-) -> bool:
-	"""
-	Determines whether NVDA should process a UIA notification event.
-	By default, events from elements with window handle value set
-	and traversable back to the desktop will be accepted.
-	Returning False will cause the event to be dropped completely.
-	Returning True means that the event will be processed, but it might still
-	be rejected later; e.g. because it isn't native UIA, because
-	shouldAcceptEvent returns False, etc.
-	"""
-	# By default, see if UIA tree can be traversed to arrive at the desktop element.
-	return bool(UIAHandler.handler.getNearestWindowHandle(sender))
-
-
 # Don't even think about proceeding in secure screens.
 # Don't worry about "cls" type.
 def disableInSecureMode(cls):  # type: ignore
