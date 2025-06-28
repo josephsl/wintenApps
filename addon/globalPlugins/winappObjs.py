af7a59f212cffb9f3be546cd6ddad75fee33e1fb
Joseph Lee <joseph.lee22590@gmail.com>
2025-06-28 12:31:16 -0600

    WinApps: remove secure mode decorator again (the global plugin does nothing)

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -14,13 +14,6 @@ import winVersion
 from logHandler import log
 
 
-# Don't even think about proceeding in secure screens.
-# Don't worry about "cls" type.
-def disableInSecureMode(cls):  # type: ignore
-	return globalPluginHandler.GlobalPlugin if globalVars.appArgs.secure else cls
-
-
-@disableInSecureMode
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def __init__(self):
 		super().__init__()
