d676ecbafcc17fafb94eb80b2be5957eaf2fe0b1
Joseph Lee <joseph.lee22590@gmail.com>
2025-07-25 11:35:20 -0600

    WinApps: replace secure mode decorator with placeholder (pass statement) comment once again.
    WinAps global plugin does nothing, therefore there is no need to check for secure mode. As a result, the placeholder (pass) statement returns (replacing secure mode decorator and associated global vars module import).

--- addon/globalPlugins/winappObjs.py
+++ addon/globalPlugins/winappObjs.py
@@ -4,16 +4,8 @@
 # Adds handlers for various UIA controls found in Windows 11.
 
 import globalPluginHandler
-import globalVars
 
 
-# Don't even think about proceeding in secure screens.
-# Don't worry about "cls" type.
-def disableInSecureMode(cls):  # type: ignore
-	return globalPluginHandler.GlobalPlugin if globalVars.appArgs.secure else cls
-
-
-@disableInSecureMode
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-	def __init__(self):
-		super().__init__()
+	# Placeholder global plugin class in case it is needed in the future.
+	pass
