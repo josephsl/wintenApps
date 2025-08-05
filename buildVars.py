873fc4e4f1815a4a51473ae374c3b8b7c359ff7d
Joseph Lee <joseph.lee22590@gmail.com>
2025-08-05 10:50:21 -0600

    BuildVars: use modernized module (part of add-on template)

--- buildVars.py
+++ buildVars.py
@@ -1,53 +1,50 @@
-# -*- coding: UTF-8 -*-
-
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
+from site_scons.site_tools.NVDATool.typings import AddonInfo, BrailleTables, SymbolDictionaries
 
 # Since some strings in `addon_info` are translatable,
 # we need to include them in the .po files.
 # Gettext recognizes only strings given as parameters to the `_` function.
-# To avoid initializing translations in this module we simply roll our own "fake" `_` function
+# To avoid initializing translations in this module we simply import a "fake" `_` function
 # which returns whatever is given to it as an argument.
-def _(arg):
-	return arg
+from site_scons.site_tools.NVDATool.utils import _
 
 
 # Add-on information variables
-addon_info = {
+addon_info = AddonInfo(
 	# add-on Name/identifier, internal for NVDA
-	"addon_name": "wintenApps",
+	addon_name="wintenApps",
 	# Add-on summary/title, usually the user visible name of the add-on
 	# Translators: Summary/title for this add-on
 	# to be shown on installation and add-on information found in add-on store
-	"addon_summary": _("Windows App Essentials"),
+	addon_summary=_("Windows App Essentials"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
-	"addon_description": _("""Improving support for various apps and controls on Windows 11"""),
+	addon_description=_("""Improving support for various apps and controls on Windows 11"""),
 	# version
-	"addon_version": "25.08",
+	addon_version="25.08",
 	# Author(s)
-	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>, Derek Riemer <driemer.riemer@gmail.com> and others",
+	addon_author="Joseph Lee <joseph.lee22590@gmail.com>, Derek Riemer <driemer.riemer@gmail.com> and others",
 	# URL for the add-on documentation support
-	"addon_url": "https://github.com/josephsl/wintenApps",
+	addon_url="https://github.com/josephsl/wintenApps",
 	# URL for the add-on repository where the source code can be found
-	"addon_sourceURL": "https://github.com/josephsl/wintenApps",
+	addon_sourceURL="https://github.com/josephsl/wintenApps",
 	# Documentation file name
-	"addon_docFileName": "readme.html",
+	addon_docFileName="readme.html",
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": "2025.2",
+	addon_minimumNVDAVersion="2025.2",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2025.2",
+	addon_lastTestedNVDAVersion="2025.2",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
-	"addon_updateChannel": None,
+	addon_updateChannel=None,
 	# Add-on license such as GPL 2
-	"addon_license": "GPL v2",
+	addon_license="GPL v2",
 	# URL for the license document the ad-on is licensed under
-	"addon_licenseURL": "https://www.gnu.org/licenses/gpl-2.0.html",
-}
-
+	addon_licenseURL="https://www.gnu.org/licenses/gpl-2.0.html",
+)
 
 # Define the python files that are the sources of your add-on.
 # You can either list every file (using ""/") as a path separator,
@@ -57,20 +54,20 @@ addon_info = {
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = [
+pythonSources: list[str] = [
 	"addon/*.py",
 	"addon/appModules/*.py",
 	"addon/globalPlugins/winappObjs.py",
 ]
 
 # Files that contain strings for translation. Usually your python sources
-i18nSources = pythonSources + ["buildVars.py"]
+i18nSources: list[str] = pythonSources + ["buildVars.py"]
 
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
 # You can either list every file (using ""/") as a path separator,
 # or use glob expressions.
-excludedFiles = [
+excludedFiles: list[str] = [
 	# Empty app module/__init__.py: kept in the repo in case needed in the future.
 	"appModules/__init__.py"
 ]
@@ -79,14 +76,14 @@ excludedFiles = [
 # If your add-on is written in a language other than english, modify this variable.
 # For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
 # You must also edit .gitignore file to specify base language files to be ignored.
-baseLanguage = "en"
+baseLanguage: str = "en"
 
 # Markdown extensions for add-on documentation
 # Most add-ons do not require additional Markdown extensions.
 # If you need to add support for markup such as tables, fill out the below list.
 # Extensions string must be of the form "markdown.extensions.extensionName"
 # e.g. "markdown.extensions.tables" to add tables.
-markdownExtensions = []
+markdownExtensions: list[str] = []
 
 # Custom braille translation tables
 # If your add-on includes custom braille tables (most will not), fill out this dictionary.
@@ -96,7 +93,7 @@ markdownExtensions = []
 # contracted (contracted (True) or uncontracted (False) braille code),
 # output (shown in output table list),
 # input (shown in input table list).
-brailleTables = {}
+brailleTables: BrailleTables = {}
 
 # Custom speech symbol dictionaries
 # Symbol dictionary files reside in the locale folder, e.g. `locale\en`, and are named `symbols-<name>.dic`.
@@ -105,4 +102,4 @@ brailleTables = {}
 # with keys inside recording the following attributes:
 # displayName (name of the speech dictionary shown to users and translatable),
 # mandatory (True when always enabled, False when not.
-symbolDictionaries = {}
+symbolDictionaries: SymbolDictionaries = {}
